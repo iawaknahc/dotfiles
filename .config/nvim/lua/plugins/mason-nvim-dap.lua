f3496e72a453e301b27ce309ff01c020c15a83c7
Louis Chan <louischan0325@gmail.com>
2023-08-08 01:47:46 +0800

    Setup nvim-dap

---
+++ .config/nvim/lua/plugins/mason-nvim-dap.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    'jay-babu/mason-nvim-dap.nvim',
+    dependencies = {
+      'williamboman/mason.nvim',
+      'mfussenegger/nvim-dap',
+    },
+    main = 'mason-nvim-dap',
+    opts = {
+      ensure_installed = {
+        'delve',
+      },
+    }
+  }
+}
