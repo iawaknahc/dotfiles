b478507f9173ebc1b0dd4df40d00e6d1dab37f6b
Louis Chan <louischan0325@gmail.com>
2023-08-20 02:09:40 +0800

    Disable coq snippets warning

--- .config/nvim/lua/plugins/coq_nvim.lua
+++ .config/nvim/lua/plugins/coq_nvim.lua
@@ -6,6 +6,9 @@ return {
       local coq_settings = {}
       coq_settings["auto_start"] = "shut-up"
       coq_settings["display.icons.mode"] = "none"
+      -- Disable the missing snippets warning.
+      -- https://github.com/ms-jpq/coq_nvim/blob/coq/docs/SNIPS.md#et-al
+      coq_settings["clients.snippets.warn"] = {}
       vim.g.coq_settings = coq_settings
     end,
   },
