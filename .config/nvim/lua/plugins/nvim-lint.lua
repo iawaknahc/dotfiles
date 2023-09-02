faeeb779b9c2b9f80edf432776e97540361ef4de
Louis Chan <louischan0325@gmail.com>
2023-09-02 23:46:04 +0800

    Ignore error when linter is not installed

--- .config/nvim/lua/plugins/nvim-lint.lua
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -13,7 +13,7 @@ function config()
   vim.api.nvim_create_autocmd("BufWritePost", {
     group = lintGroup,
     callback = function()
-      lint.try_lint()
+      lint.try_lint(nil, { ignore_errors = true })
     end,
   })
 end
