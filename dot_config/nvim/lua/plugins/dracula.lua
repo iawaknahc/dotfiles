1d91c7aba774f396fc10ddfe11dd4f3062b93b81
Louis Chan <louischan0325@gmail.com>
2024-05-24 02:22:12 +0800

    Make inlay hint less visually prominent

--- dot_config/nvim/lua/plugins/dracula.lua
+++ dot_config/nvim/lua/plugins/dracula.lua
@@ -5,6 +5,8 @@ return {
     lazy = false,
     config = function()
       vim.cmd [[colorscheme dracula]]
+      -- Make inlay hint less visually prominent.
+      vim.cmd [[highlight DraculaInlayHint cterm=italic gui=italic ctermbg=NONE guibg=NONE]]
     end,
   },
 }
