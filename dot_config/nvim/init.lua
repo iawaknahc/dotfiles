06bd9f44bb49e46639a84aae635ce6ec0f661de1
Louis Chan <louischan0325@gmail.com>
2024-06-07 03:23:29 +0800

    Make init.lua not load .vimrc anymore

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,17 +1,79 @@
--- load .vimrc
+-- neovim defaults to filetype plugin indent on
 vim.cmd [[
-" :help nvim-from-vim
-set runtimepath^=~/.vim runtimepath+=~/.vim/after
-let &packpath = &runtimepath
-source ~/.vimrc
+filetype on
+filetype plugin on
+filetype indent off
 ]]
+
+-- Security
+-- https://github.com/numirias/security/blob/master/doc/2019-06-04_ace-vim-neovim.md
+vim.o.modeline = false
+
+-- Look
+vim.o.signcolumn = "yes"
+vim.o.number = true
+vim.o.list = true
+-- lead:. is taken from the help of neovim.
+-- trail:- is the default of neovim.
+-- nbsp:+ is the default of neovim.
+-- tab:>  is the default of neovim. We change it to tab:>_ so that
+-- the space is visible and distinguishable from leading spaces.
+vim.o.listchars = "tab:>_,lead:.,trail:-,nbsp:+"
+vim.o.breakindent = true
 
 -- colorcolumn
 local colorcolumn = {}
 for i = 1, 100 do
   table.insert(colorcolumn, "+" .. i)
 end
-vim.wo.colorcolumn = table.concat(colorcolumn, ",")
+vim.o.colorcolumn = table.concat(colorcolumn, ",")
+
+-- Completion
+vim.o.completeopt = "menu,menuone,noselect"
+
+-- Command completion
+vim.o.wildmode = "longest:full,full"
+
+-- Editing
+vim.o.backup = false
+vim.o.writebackup = false
+vim.o.swapfile = false
+vim.o.scrolloff = 5
+vim.o.foldenable = false
+vim.o.clipboard = "unnamed"
+vim.o.fixendofline = false
+
+-- Search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Mapping
+vim.g.mapleader = " "
+vim.keymap.set("n", "<Leader><Space>", ":set hlsearch!<CR>", { remap = false })
+
+-- Command
+vim.api.nvim_create_user_command("Space", function(t)
+  local n = tonumber(t.fargs[1])
+  if n ~= nil then
+    vim.bo.tabstop = n
+    vim.bo.shiftwidth = n
+    vim.bo.softtabstop = n
+    vim.bo.expandtab = true
+  end
+end, {
+  nargs = 1,
+})
+vim.api.nvim_create_user_command("Tab", function(t)
+  local n = tonumber(t.fargs[1])
+  if n ~= nil then
+    vim.bo.tabstop = n
+    vim.bo.shiftwidth = n
+    vim.bo.softtabstop = n
+    vim.bo.expandtab = false
+  end
+end, {
+  nargs = 1,
+})
 
 -- Set nvim-specific options
 vim.diagnostic.config {
@@ -25,6 +87,28 @@ vim.diagnostic.config {
 }
 
 -- Set up autocommands
+-- By default, filetype.vim treats *.env as sh
+-- We do NOT want to run any before-save fix on *.env
+-- For example, some envvars may have trailing whitespaces we do want to preserve.
+local dotEnvGroup = vim.api.nvim_create_augroup("MyDotEnv", { clear = true })
+vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
+  pattern = "*.env",
+  callback = function()
+    vim.bo.filetype = ""
+  end,
+  group = dotEnvGroup,
+})
+
+local spellCheckGroup = vim.api.nvim_create_augroup("MySpellCheck", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "gitcommit", "text" , "markdown" },
+  callback = function()
+    vim.wo.spell = true
+    vim.bo.spelllang = "en_us"
+  end,
+  group = spellCheckGroup,
+})
+
 local yankGroup = vim.api.nvim_create_augroup("MyYankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
   pattern = "*",
