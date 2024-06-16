4e0cf6e824741a64236e6ad95f22d9530a84d17c
Louis Chan <louischan0325@gmail.com>
2024-06-16 16:09:15 +0800

    Add some missing highlight groups for Dracula

--- dot_config/nvim/lua/exact_plugins/dracula.lua
+++ dot_config/nvim/lua/exact_plugins/dracula.lua
@@ -1,3 +1,44 @@
+-- As of 2024-06-16, the following highlight groups are not linked to Dracula.
+--
+-- @lsp.type.modifier links to non-existent @type.qualifier
+-- Added          xxx ctermfg=10 guifg=NvimLightGreen
+-- Changed        xxx ctermfg=14 guifg=NvimLightCyan
+-- CmpItemAbbrDeprecatedDefault xxx guifg=NvimLightGrey4
+-- CmpItemKindDefault xxx guifg=NvimLightCyan
+-- Conceal        xxx ctermfg=4 guifg=#8be9fd
+-- CurSearch      xxx ctermfg=0 ctermbg=11 guifg=NvimDarkGrey1 guibg=NvimLightYellow
+-- Cursor         xxx guifg=bg guibg=fg
+-- CursorLine     xxx ctermbg=0 guibg=#424450
+-- DiagnosticDeprecated xxx cterm=strikethrough gui=strikethrough guisp=NvimLightRed
+-- DiagnosticOk   xxx ctermfg=10 guifg=NvimLightGreen
+-- DiagnosticUnderlineOk xxx cterm=underline gui=underline guisp=NvimLightGreen
+-- FloatShadow    xxx ctermbg=0 guibg=NvimDarkGrey4 blend=80
+-- FloatShadowThrough xxx ctermbg=0 guibg=NvimDarkGrey4 blend=100
+-- IblScope       xxx ctermfg=8 guifg=#6272a4
+-- LineNr         xxx ctermfg=8 guifg=#6272a4
+-- MatchParen     xxx cterm=underline ctermfg=2 gui=underline guifg=#50fa7b
+-- ModeMsg        xxx ctermfg=10 guifg=NvimLightGreen
+-- Normal         xxx ctermfg=7 guifg=#f8f8f2 guibg=#282a36
+-- NormalFloat    xxx guibg=NvimDarkGrey1
+-- PmenuMatch     xxx ctermfg=117 ctermbg=235 guifg=#8be9fd guibg=#21222c
+-- PmenuMatchSel  xxx ctermfg=117 ctermbg=239 guifg=#8be9fd guibg=#44475a
+-- QuickFixLine   xxx ctermfg=14 guifg=NvimLightCyan
+-- RedrawDebugClear xxx ctermfg=0 ctermbg=11 guibg=NvimDarkYellow
+-- RedrawDebugComposed xxx ctermfg=0 ctermbg=10 guibg=NvimDarkGreen
+-- RedrawDebugNormal xxx cterm=reverse gui=reverse
+-- RedrawDebugRecompose xxx ctermfg=0 ctermbg=9 guibg=NvimDarkRed
+-- Removed        xxx ctermfg=9 guifg=NvimLightRed
+-- SignColumn     xxx ctermfg=8 guifg=#6272a4
+-- StatusLine     xxx cterm=bold ctermbg=8 gui=bold guibg=#424450
+-- StatusLineNC   xxx ctermbg=8 guibg=#343746
+-- StatusLineTerm xxx cterm=bold ctermbg=8 gui=bold guibg=#424450
+-- StatusLineTermNC xxx ctermbg=8 guibg=#343746
+-- TermCursor     xxx cterm=reverse gui=reverse
+-- WildMenu       xxx cterm=bold,reverse ctermfg=6 gui=bold guifg=#282a36 guibg=#bd93f9
+-- WinBar         xxx cterm=bold gui=bold guifg=NvimLightGrey4 guibg=NvimDarkGrey1
+-- WinBarNC       xxx cterm=bold guifg=NvimLightGrey4 guibg=NvimDarkGrey1
+-- lCursor        xxx guifg=bg guibg=fg
+
 return {
   {
     "dracula/vim",
@@ -67,6 +108,13 @@ highlight LineNr ctermfg=8
 highlight SignColumn ctermfg=8
 highlight MatchParen ctermfg=2
 highlight Conceal ctermfg=4
+
+highlight Added guifg=#50fa7b
+highlight Removed guifg=#ff5555
+highlight Changed guifg=#ffb86c
+highlight DiagnosticDeprecated guisp=#ff5555
+highlight DiagnosticOk guifg=#50fa7b
+highlight DiagnosticUnderlineOk guisp=#50fa7b
       ]]
 
     end,
