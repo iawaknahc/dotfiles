5e950c93d7a4824b733ec09eff47a24413453d48
Louis Chan <louischan0325@gmail.com>
2024-06-23 01:39:29 +0800

    Add description in dap mappings

--- dot_config/nvim/lua/exact_plugins/nvim-dap.lua
+++ dot_config/nvim/lua/exact_plugins/nvim-dap.lua
@@ -2,23 +2,44 @@ return {
   {
     "mfussenegger/nvim-dap",
     keys = {
-      { "<F5>" },
-      { "<F10>" },
-      { "<F11>" },
-      { "<F12>" },
-      { "<Leader>R" },
-      { "<Leader>K" },
+      {
+        "<F5>", function()
+          require("dap").continue()
+        end,
+        desc = "Debugger continue",
+      },
+      {
+        "<F10>", function()
+          require("dap").step_over()
+        end,
+        desc = "Debugger step over",
+      },
+      {
+        "<F11>", function()
+          require("dap").step_into()
+        end,
+        desc = "Debugger step into",
+      },
+      {
+        "<F12>", function()
+          require("dap").step_out()
+        end,
+        desc = "Debugger step out",
+      },
+      {
+        "<Leader>R", function()
+          require("dap").repl.toggle()
+        end,
+        desc = "Debugger toggle REPL",
+      },
+      {
+        "<Leader>K", function()
+          require("dap.ui.widgets").hover()
+        end,
+        mode = {"n", "v"},
+        desc = "Debugger hover",
+      },
     },
-    config = function()
-      local dap = require("dap")
-      local widgets = require("dap.ui.widgets")
-      local map_opts = { noremap = true }
-      vim.keymap.set("n", "<F5>", dap.continue, map_opts)
-      vim.keymap.set("n", "<F10>", dap.step_over, map_opts)
-      vim.keymap.set("n", "<F11>", dap.step_into, map_opts)
-      vim.keymap.set("n", "<F12>", dap.step_out, map_opts)
-      vim.keymap.set("n", "<Leader>R", dap.repl.toggle, map_opts)
-      vim.keymap.set({ "n", "v" }, "<Leader>K", widgets.hover, map_opts)
-    end,
+    config = function() end,
   },
 }
