de527f9029da3b3cbb97da76005e2eda484dd2ec
Louis Chan <louischan0325@gmail.com>
2024-06-23 01:50:52 +0800

    Switch from nvim-navic to nvim-treesitter-context

--- dot_config/nvim/lua/exact_plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/exact_plugins/nvim-treesitter.lua
@@ -82,4 +82,12 @@ return {
       },
     },
   },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    lazy = false,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = true,
+  },
 }
