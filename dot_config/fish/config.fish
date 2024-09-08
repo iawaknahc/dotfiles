849b62dbd427bbf041533922b0a3f3fc4c50659a
Louis Chan <louischan0325@gmail.com>
2024-09-08 12:36:31 +0800

    Add fish function pe

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -128,15 +128,6 @@ if test -r "$HOME/.opam/opam-init/init.fish"
   source "$HOME/.opam/opam-init/init.fish"
 end
 
-# python
-if test -x "$(command -v python3)"
-  alias jsontool='python3 -m json.tool --no-ensure-ascii'
-  alias percent_encode_path_segment='python3 -c \'import sys,urllib.parse as u;[print(u.quote(l,safe=\\\':@$&+=\\\'))for l in sys.stdin.read().splitlines()]\''
-  alias percent_decode_path_segment='python3 -c \'import sys,urllib.parse as u;[print(u.unquote(l))for l in sys.stdin.read().splitlines()]\''
-  alias percent_encode_query_component='python3 -c \'import sys,urllib.parse as u;[print(u.quote_plus(l))for l in sys.stdin.read().splitlines()]\''
-  alias percent_decode_query_component='python3 -c \'import sys,urllib.parse as u;[print(u.unquote_plus(l))for l in sys.stdin.read().splitlines()]\''
-end
-
 # rust
 if test -d "$HOME/.cargo"
   fish_add_path -P "$HOME/.cargo/bin"
