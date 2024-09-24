89564df991dca431c1aa3dd69c02e1ba5725aeeb
Louis Chan <louischan0325@gmail.com>
2024-09-25 00:22:58 +0800

    Switch to vim-gitgutter

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -48,6 +48,10 @@ vim.opt.wildmode = { "longest:full", "full" }
 vim.o.backup = false
 vim.o.writebackup = false
 vim.o.swapfile = false
+-- This controls how often the swapfile is written.
+-- This also controls how often vim-gitgutter updates the signs.
+-- :h updatetime
+vim.o.updatetime=100
 vim.o.scrolloff = 5
 vim.o.foldenable = false
 vim.opt.clipboard:append({ "unnamed" })
---
+++ dot_config/nvim/lua/exact_plugins/vim-gitgutter.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    -- Previously I used gitsigns.nvim
+    -- But it does more than showing signs.
+    "airblade/vim-gitgutter",
+    enabled = true,
+    event = {
+      "VeryLazy",
+    },
+    init = function()
+    end,
+  },
+}
