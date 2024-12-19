4ac0ac5c6ae44e62eca54e713f33408494b992db
Louis Chan <louischan0325@gmail.com>
2024-12-19 21:46:33 +0800

    Make nvim-highlight a script file

--- home.nix
+++ home.nix
@@ -468,19 +468,6 @@ lib.mkMerge [
         ];
     }
   )
-  {
-    home.packages = [
-      # -Es causes :print prints to stdout, instead of showing the output with the builtin pager.
-      # -u reverts the effect of -Es, because the presence of -Es implies user config is not loaded.
-      # :set nonumber turns off line number.
-      # :put =execute('highlight') puts the output of the Ex command into the buffer.
-      # :g/^$/d deletes any empty lines.
-      # :print finally prints to stdout.
-      (pkgs.writeShellScriptBin "nvim-highlight" ''
-        nvim -Es -u ~/.config/nvim/init.lua +'set nonumber' +"put =execute('highlight')" +'g/^$/d' +%print
-      '')
-    ];
-  }
 
   # tlrc
   # TLRC_CONFIG was implemented but not released yet.
