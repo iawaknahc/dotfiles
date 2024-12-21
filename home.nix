8c24eb067c3c5f3f223cedc8897e3426e8f47c75
Louis Chan <louischan0325@gmail.com>
2024-12-21 20:55:45 +0800

    Install official nushell plugins

--- home.nix
+++ home.nix
@@ -455,6 +455,13 @@ lib.mkMerge [
     programs.nushell.shellAliases = config.home.shellAliases;
     programs.nushell.envFile.source = ./.config/nushell/env.nu;
     programs.nushell.configFile.source = ./.config/nushell/config.nu;
+    # Install official plugins.
+    programs.nushell.plugins = [
+      pkgs.nushellPlugins.polars
+      pkgs.nushellPlugins.formats
+      pkgs.nushellPlugins.gstat
+      pkgs.nushellPlugins.query
+    ];
     programs.carapace.enable = true;
     programs.carapace.enableBashIntegration = false;
     programs.carapace.enableFishIntegration = false;
