38269a9cf8491eed81e90fd8de6e2f611524fcfb
Louis Chan <louischan0325@gmail.com>
2024-12-22 16:40:05 +0800

    Use starship to configure prompt

--- home.nix
+++ home.nix
@@ -350,10 +350,6 @@ lib.mkMerge [
 
       # Turn on vi mode
       set -o vi
-
-      # Configure prompt
-      export PS1="[$SHLVL] \$ "
-      export PS2='> '
     '';
     programs.bash.profileExtra = ''
       echo "sourcing .profile"
@@ -391,10 +387,6 @@ lib.mkMerge [
       export __ETC_PROFILE_NIX_SOURCED="$__ETC_PROFILE_NIX_SOURCED"
       # The above script exports XDG_DATA_DIRS, claiming to provide bash completion.
       unset XDG_DATA_DIRS
-
-      # Configure prompt
-      export PS1="[$SHLVL] \$ "
-      export PS2='> '
     '';
   }
 
@@ -467,6 +459,53 @@ lib.mkMerge [
     programs.carapace.enableFishIntegration = false;
     programs.carapace.enableNushellIntegration = true;
     programs.carapace.enableZshIntegration = false;
+  }
+
+  # starship
+  {
+    programs.starship.enable = true;
+    programs.starship.enableBashIntegration = true;
+    programs.starship.enableFishIntegration = true;
+    programs.starship.enableNushellIntegration = false;
+    programs.starship.enableZshIntegration = true;
+    programs.starship.settings = {
+      add_newline = false;
+      follow_symlinks = false;
+      format = "$shell$shlvl$character $status ";
+      shell = {
+        disabled = false;
+        format = "[$indicator]($style)";
+        bash_indicator = "bash";
+        fish_indicator = "fish";
+        nu_indicator = "nu";
+        unknown_indicator = "unknown";
+      };
+      character = {
+        disabled = false;
+        format = "$symbol";
+        success_symbol = "[I](bold green)";
+        error_symbol = "[I](bold green)";
+        vimcmd_symbol = "[N](bold red)";
+        vimcmd_replace_one_symbol = "[R](bold green)";
+        vimcmd_replace_symbol = "[R](bold cyan)";
+        vimcmd_visual_symbol = "[V](bold purple)";
+      };
+      shlvl = {
+        disabled = false;
+        threshold = 0;
+        format = "[$shlvl](bold yellow)";
+      };
+      status = {
+        disabled = false;
+        format = "$symbol";
+        success_symbol = "\\$";
+        symbol = "[$status \\$](bold red)";
+        not_executable_symbol = "[$status \\$](bold red)";
+        not_found_symbol = "[$status \\$](bold red)";
+        sigint_symbol = "[$status \\$](bold red)";
+        signal_symbol = "[$status \\$](bold red)";
+      };
+    };
   }
 
   # direnv
