16ad41c8c4620823bc30e727f13d0091cee6647e
Louis Chan <louischan0325@gmail.com>
2024-12-23 00:17:14 +0800

    Install json5

--- home.nix
+++ home.nix
@@ -141,6 +141,19 @@ lib.mkMerge [
       pkgs.hadolint
       pkgs.imagemagick
       pkgs.jq
+      # As of 2024-12-23, there is no official json5 package.
+      # So let's build it ourselves.
+      (pkgs.buildNpmPackage {
+        pname = "json5";
+        version = "2.2.3";
+        src = pkgs.fetchFromGitHub {
+          owner = "json5";
+          repo = "json5";
+          rev = "v2.2.3";
+          hash = "sha256-ZOF1aLzs4AREy0PgPWexBYB5rL81UOVRPDcnSBOghiE=";
+        };
+        npmDepsHash = "sha256-ZGOo77uph5JeRGUB0c+BZOg04hXnvpXM95zx4ByX2E4=";
+      })
       pkgs.lua-language-server
       pkgs.mkcert
       pkgs.navi
