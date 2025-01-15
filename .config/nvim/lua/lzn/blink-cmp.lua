01abad943528b3e8658540c9903f40f9659689bc
Louis Chan <louischan0325@gmail.com>
2025-01-15 21:17:29 +0800

    Make buffer not a fallback source

--- .config/nvim/lua/lzn/blink-cmp.lua
+++ .config/nvim/lua/lzn/blink-cmp.lua
@@ -2,7 +2,16 @@ require("lz.n").load {
   "blink.cmp",
   event = { "InsertEnter", "CmdlineEnter" },
   after = function()
+    -- Do not make buffer a fallback source
+    -- I want to complete with buffer even LSP source has items.
+    -- https://github.com/Saghen/blink.cmp/blob/v0.10.0/lua/blink/cmp/config/sources.lua
+    local presets = require("blink.cmp.config.sources")
+    local preset_default = presets.default
+    preset_default.providers.lsp.fallbacks = nil
+    preset_default.providers.path.fallbacks = nil
+
     require("blink.cmp").setup({
+      sources = preset_default,
       completion = {
         list = {
           selection = {
