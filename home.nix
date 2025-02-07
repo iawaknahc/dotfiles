73345508d76890f3447477684bda347a45069ac9
Louis Chan <louischan0325@gmail.com>
2025-02-08 02:14:01 +0800

    Delete bat

--- home.nix
+++ home.nix
@@ -68,7 +68,6 @@
     ./home-manager/mkcert.nix
     ./home-manager/gpg.nix
     ./home-manager/navi.nix
-    ./home-manager/bat.nix
     ./home-manager/delta.nix
     ./home-manager/git.nix
     ./home-manager/pip.nix
