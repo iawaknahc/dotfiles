a9fae4c5d9edc08a4b7f41cbc0771c843897901c
Louis Chan <louischan0325@gmail.com>
2025-02-08 23:43:56 +0800

    Configure carapace without home-manager module

--- home-manager/shell-completion.nix
+++ home-manager/shell-completion.nix
@@ -1,14 +1,43 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
+  # Do not use programs.carapace because we do not want to unset the shell integrations.
   home.sessionVariables = {
     "CARAPACE_BRIDGES" = "zsh,fish,bash,inshellisense";
   };
   home.packages = with pkgs; [
     inshellisense
+    carapace
   ];
-  programs.carapace.enable = true;
-  programs.carapace.enableBashIntegration = true;
-  programs.carapace.enableFishIntegration = true;
-  programs.carapace.enableNushellIntegration = true;
-  programs.carapace.enableZshIntegration = true;
+
+  programs.bash.initExtra = ''
+    source <(carapace carapace)
+  '';
+
+  programs.fish.interactiveShellInit = ''
+    carapace _carapace | source
+  '';
+  xdg.configFile."fish/completions" = {
+    enable = true;
+    recursive = true;
+    source =
+      pkgs.runCommandLocal "carapace-fish-issue-185"
+        {
+          buildInputs = [
+            pkgs.carapace
+          ];
+        }
+        ''
+          mkdir $out
+          ${pkgs.carapace}/bin/carapace --list | awk '{ print $1 }' | xargs -I{} touch $out/{}.fish
+        '';
+  };
+
+  programs.nushell.extraConfig = ''
+    mkdir ($nu.data-dir | path join "vendor/autoload")
+    carapace _carapace nushell | save --force ($nu.data-dir | path join "vendor/autoload/carapace.nu")
+  '';
+  programs.zsh.initExtra = ''
+    zstyle ':completion:*' format $'\e[2;37mCompleting %d\e[m'
+    source <(carapace _carapace)
+  '';
 }
