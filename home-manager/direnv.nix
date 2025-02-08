6abdd0575531526e7d78708d738041f6417939d1
Louis Chan <louischan0325@gmail.com>
2025-02-09 00:15:13 +0800

    Configure direnv without home-manager module

--- home-manager/direnv.nix
+++ home-manager/direnv.nix
@@ -1,7 +1,36 @@
-{ ... }:
+{ pkgs, lib, ... }:
 {
-  # Install the binary direnv.
-  programs.direnv.enable = true;
-  # Install nix-direnv to ~/.config/direnv/lib/hm-nix-direnv.sh
-  programs.direnv.nix-direnv.enable = true;
+  home.packages = with pkgs; [
+    direnv
+    nix-direnv
+  ];
+  xdg.configFile."direnv/lib/hm-nix-direnv.sh" = {
+    enable = true;
+    source = "${pkgs.nix-direnv}/share/nix-direnv/direnvrc";
+  };
+
+  programs.bash.initExtra = lib.mkAfter ''
+    eval "$(direnv hook bash)"
+  '';
+
+  programs.fish.interactiveShellInit = lib.mkAfter ''
+    direnv hook fish | source
+  '';
+
+  programs.nushell.extraConfig = lib.mkAfter ''
+    $env.config = ($env.config | upsert hooks.env_change.PWD {|config|
+      let val = ($config | get -i hooks.env_change.PWD)
+      if $val == null {
+        $val | append {|| direnv export json | from json | default {} | load-env }
+      } else {
+        [
+          {|| direnv export json | from json | default {} | load-env }
+        ]
+      }
+    })
+  '';
+
+  programs.zsh.initExtra = lib.mkAfter ''
+    eval "$(direnv hook zsh)"
+  '';
 }
