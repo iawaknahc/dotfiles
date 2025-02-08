cf947e89c03b5eec1eb2d20ca35c3c8246c15ef0
Louis Chan <louischan0325@gmail.com>
2025-02-08 23:14:59 +0800

    Configure starship without home-manager module

--- home-manager/starship.nix
+++ home-manager/starship.nix
@@ -1,59 +1,24 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  programs.starship.enable = true;
-  programs.starship.enableBashIntegration = true;
-  programs.starship.enableFishIntegration = true;
-  programs.starship.enableNushellIntegration = false;
-  programs.starship.enableZshIntegration = true;
-  programs.starship.settings = {
-    add_newline = false;
-    follow_symlinks = false;
-    format = "$kubernetes$direnv$shell$shlvl$character $status ";
-    shell = {
-      disabled = false;
-      format = "[$indicator]($style)";
-      bash_indicator = "bash";
-      fish_indicator = "fish";
-      nu_indicator = "nu";
-      unknown_indicator = "unknown";
-    };
-    character = {
-      disabled = false;
-      format = "$symbol";
-      success_symbol = "[I](bold green)";
-      error_symbol = "[I](bold green)";
-      vimcmd_symbol = "[N](bold red)";
-      vimcmd_replace_one_symbol = "[R](bold green)";
-      vimcmd_replace_symbol = "[R](bold cyan)";
-      vimcmd_visual_symbol = "[V](bold purple)";
-    };
-    # It is a known issue that SHLVL does not work in bash
-    # See https://github.com/starship/starship/issues/2407#issuecomment-1433682262
-    # See https://github.com/starship/starship/pull/4912
-    shlvl = {
-      disabled = false;
-      threshold = 0;
-      format = "[$shlvl](bold yellow)";
-    };
-    status = {
-      disabled = false;
-      format = "$symbol";
-      success_symbol = "\\$";
-      symbol = "[$status \\$](bold red)";
-      not_executable_symbol = "[$status \\$](bold red)";
-      not_found_symbol = "[$status \\$](bold red)";
-      sigint_symbol = "[$status \\$](bold red)";
-      signal_symbol = "[$status \\$](bold red)";
-    };
-    direnv = {
-      disabled = false;
-      format = "[$loaded]($style)";
-      loaded_msg = ".";
-      unloaded_msg = "";
-    };
-    kubernetes = {
-      disabled = false;
-      format = "[\\($context\\)]($style)";
-    };
+  # Do not use programs.starship because we do not want to unset the shell integrations.
+  home.packages = with pkgs; [
+    starship
+  ];
+  xdg.configFile."starship.toml" = {
+    enable = true;
+    source = ../.config/starship.toml;
   };
+  programs.bash.initExtra = ''
+    eval "$(starship init bash)"
+  '';
+  programs.fish.interactiveShellInit = ''
+    starship init fish | source
+  '';
+  programs.nushell.extraConfig = ''
+    mkdir ($nu.data-dir | path join "vendor/autoload")
+    starship init nu | save --force ($nu.data-dir | path join "vendor/autoload/starship.nu")
+  '';
+  programs.zsh.initExtra = ''
+    eval "$(starship init zsh)"
+  '';
 }
