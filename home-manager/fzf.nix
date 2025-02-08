269095b9ac29a78ef9b3d933180ce380918735d4
Louis Chan <louischan0325@gmail.com>
2025-02-08 22:58:34 +0800

    Configure fzf without using home-manager module

--- home-manager/fzf.nix
+++ home-manager/fzf.nix
@@ -1,14 +1,16 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  programs.fzf.enable = true;
-  programs.fzf.defaultCommand = "true";
-  programs.fzf.defaultOptions = [
-    "--with-shell='sh -c'"
-    "--height=40%"
-    "--layout=reverse"
-    "--border"
+  # Do not use programs.fzf because we do not want to unset the shell integrations.
+  home.packages = with pkgs; [
+    fzf
   ];
-  programs.fzf.enableBashIntegration = false;
-  programs.fzf.enableZshIntegration = false;
-  programs.fzf.enableFishIntegration = false;
+  home.sessionVariables = {
+    FZF_DEFAULT_COMMAND = "true";
+    FZF_DEFAULT_OPTS = builtins.toString [
+      "--with-shell='sh -c'"
+      "--height=40%"
+      "--layout=reverse"
+      "--border"
+    ];
+  };
 }
