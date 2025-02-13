1a8dddde35fc604b62c7cee6ac97d8b8d910e585
Louis Chan <louischan0325@gmail.com>
2025-02-13 19:46:50 +0800

    Document elvish does not support Kitty Keyboard protocol

--- home-manager/elvish.nix
+++ home-manager/elvish.nix
@@ -6,5 +6,10 @@
     # Note that this must appear after we have set up the PATH,
     # otherwise, `command -v elvish` is an empty string.
     set-env SHELL (command -v elvish)
+
+    # We cannot bind Ctrl-J because Elvish does not support Kitty keyboard protocol
+    # https://github.com/elves/elvish/issues/1244
+    set edit:listing:binding[Ctrl-N] = { edit:listing:down }
+    set edit:listing:binding[Ctrl-P] = { edit:listing:up }
   '';
 }
