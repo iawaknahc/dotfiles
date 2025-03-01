21aab834f38906079e0f22308cc24e397cc254a5
Louis Chan <louischan0325@gmail.com>
2025-03-01 11:19:52 +0800

    Document my experience with ble.sh

--- home-manager/bash.nix
+++ home-manager/bash.nix
@@ -3,7 +3,18 @@
 { pkgs, ... }:
 {
   programs.bash.enable = true;
-  home.packages = with pkgs; [ bash-completion ];
+  home.packages = with pkgs; [
+    bash-completion
+    # On 2025-03-01, I tried ble.sh. It was 0.4.0-devel3
+    # It did not work well for me, for the following reasons:
+    #
+    # 1. The vi mode setup was documented at GitHub Wiki, so it was hard to view the
+    #    documentation that was applicable to 0.4.0-devel3
+    #    I had mode shown with Starship so I wanted to disable the mode line,
+    #    but the poor documentation did not allow me to do so.
+    # 2. The highlight used colors other than my terminal colors. I did not like this behavior.
+    blesh
+  ];
   programs.bash.bashrcExtra = ''
     if shopt -q login_shell; then
       echo "login shell: true"
