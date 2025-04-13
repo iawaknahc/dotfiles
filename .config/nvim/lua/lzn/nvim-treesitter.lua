b087ec008264c318caff0482675ae2ddf38242ed
Louis Chan <louischan0325@gmail.com>
2025-04-14 00:03:47 +0800

    Set up more treesitter text objects

--- .config/nvim/lua/lzn/nvim-treesitter.lua
+++ .config/nvim/lua/lzn/nvim-treesitter.lua
@@ -29,31 +29,76 @@ local textobjects = {
   select = {
     enable = true,
     keymaps = {
-      -- a stands for argument.
-      ["aa"] = "@parameter.outer",
-      ["ia"] = "@parameter.inner",
+      -- a stands for assignment.
+      ["aa"] = { query = "@assignment.outer", desc = "Select around assignment" },
+      ["ia"] = { query = "@assignment.inner", desc = "Select inner assignment" },
+      -- b stands for block.
+      -- Override :h ab and :h ib
+      ["ab"] = { query = "@block.outer", desc = "Select around block" },
+      ["ib"] = { query = "@block.inner", desc = "Select inner block" },
+      -- c stands for class.
+      ["ac"] = { query = "@class.outer", desc = "Select around class" },
+      ["ic"] = { query = "@class.inner", desc = "Select inner class" },
+      -- l stands for loop.
+      ["al"] = { query = "@loop.outer", desc = "Select around loop" },
+      ["il"] = { query = "@loop.inner", desc = "Select inner loop" },
       -- m stands for method, resembles the builtin [m
-      ["am"] = "@function.outer",
-      ["im"] = "@function.inner",
+      ["am"] = { query = "@function.outer", desc = "Select around method or function" },
+      ["im"] = { query = "@function.inner", desc = "Select inner method or function" },
+      -- p stands for parameter.
+      -- Override :h ap and :h ip
+      ["ap"] = { query = "@parameter.outer", desc = "Select around parameter" },
+      ["ip"] = { query = "@parameter.inner", desc = "Select inner parameter" },
+      -- s stands for statement.
+      ["as"] = { query = "@statement.outer", desc = "Select around statement" },
+      ["is"] = { query = "@statement.inner", desc = "Select inner statement" },
     },
   },
   move = {
     enable = true,
     goto_next_start = {
-      ["]a"] = "@parameter.outer",
-      ["]m"] = "@function.outer",
+      ["]a"] = { query = "@assignment.outer", desc = "Next start of assignment" },
+      ["]b"] = { query = "@block.outer", desc = "Next start of block" },
+      ["]c"] = { query = "@class.outer", desc = "Next start of class" },
+      ["]l"] = { query = "@loop.outer", desc = "Next start of loop" },
+      -- Override :h ]m
+      ["]m"] = { query = "@function.outer", desc = "Next start of method or function" },
+      ["]p"] = { query = "@parameter.outer", desc = "Next start of parameter" },
+      -- Override :h ]s
+      ["]s"] = { query = "@statement.outer", desc = "Next start of statement" },
     },
     goto_next_end = {
-      ["]A"] = "@parameter.outer",
-      ["]M"] = "@function.outer",
+      ["]A"] = { query = "@assignment.outer", desc = "Next end of assignment" },
+      ["]B"] = { query = "@block.outer", desc = "Next end of block" },
+      ["]C"] = { query = "@class.outer", desc = "Next end of class" },
+      ["]L"] = { query = "@loop.outer", desc = "Next end of loop" },
+      -- Override :h ]M
+      ["]M"] = { query = "@function.outer", desc = "Next end of method or function" },
+      ["]P"] = { query = "@parameter.outer", desc = "Next end of parameter" },
+      -- Override :h ]S
+      ["]S"] = { query = "@statement.outer", desc = "Next end of statement" },
     },
     goto_previous_start = {
-      ["[a"] = "@parameter.outer",
-      ["[m"] = "@function.outer",
+      ["[a"] = { query = "@assignment.outer", desc = "Previous start of assignment" },
+      ["[b"] = { query = "@block.outer", desc = "Previous start of block" },
+      ["[c"] = { query = "@class.outer", desc = "Previous start of class" },
+      ["[l"] = { query = "@loop.outer", desc = "Previous start of loop" },
+      -- Override :h [m
+      ["[m"] = { query = "@function.outer", desc = "Previous start of method or function" },
+      ["[p"] = { query = "@parameter.outer", desc = "Previous start of parameter" },
+      -- Override :h [s
+      ["[s"] = { query = "@statement.outer", desc = "Previous start of statement" },
     },
     goto_previous_end = {
-      ["[A"] = "@parameter.outer",
-      ["[M"] = "@function.outer",
+      ["[A"] = { query = "@assignment.outer", desc = "Previous end of assignment" },
+      ["[B"] = { query = "@block.outer", desc = "Previous end of block" },
+      ["[C"] = { query = "@class.outer", desc = "Previous end of class" },
+      ["[L"] = { query = "@loop.outer", desc = "Previous end of loop" },
+      -- Override :h [M
+      ["[M"] = { query = "@function.outer", desc = "Previous end of method or function" },
+      ["[P"] = { query = "@parameter.outer", desc = "Previous end of parameter" },
+      -- Override :h [S
+      ["[S"] = { query = "@statement.outer", desc = "Previous end of statement" },
     },
   },
 }
