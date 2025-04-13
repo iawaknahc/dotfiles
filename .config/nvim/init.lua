7e1441583818dd42fe1af2ff32b44457532dff4e
Louis Chan <louischan0325@gmail.com>
2025-04-14 01:17:33 +0800

    Assume search in current line in in visual mode

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -83,6 +83,7 @@ vim.keymap.set("n", "gv", "<Nop>", { desc = "Disable :h gv" })
 vim.keymap.set("n", "<C-l>", "<Cmd>set hlsearch!<Bar>diffupdate<Bar>normal! <C-L><CR>", {
   desc = ":h CTRL-L-default with nohlsearch changed to hlsearch!",
 })
+vim.keymap.set("v", "/", "<Esc>/<Bslash>%.l", { desc = "/\\%.l" })
 
 -- Command
 vim.api.nvim_create_user_command("Space", function(t)
