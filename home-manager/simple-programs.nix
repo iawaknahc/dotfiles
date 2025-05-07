124a70dd1d674787059274f1c42b6bfa2072d8f9
Louis Chan <louischan0325@gmail.com>
2025-05-07 23:44:36 +0800

    Install jo

--- home-manager/simple-programs.nix
+++ home-manager/simple-programs.nix
@@ -11,16 +11,25 @@
     fastmod
     ffmpeg
     fnlfmt
+
     # It provides gh-md-toc
     github-markdown-toc-go
+
     # pprof requires this graph visualization software to generate graphs.
     graphviz
+
     hadolint
     imagemagick
+
+    # https://github.com/jpmens/jo
+    jo
+
     jq
+
     # Metadata anonymisation toolkit 2
     # https://0xacab.org/jvoisin/mat2
     mat2
+
     nixfmt-rfc-style
     prettierd
     shellcheck
