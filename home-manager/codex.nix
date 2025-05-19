86e6f3eb151875bf855300234fbbd4444c2b881e
Louis Chan <louischan@oursky.com>
2025-05-19 22:33:34 +0800

    Unbreak codex

--- home-manager/codex.nix
+++ home-manager/codex.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   home.sessionVariablesExtra = ''
     if [ -z "$OPENROUTER_API_KEY" ]; then
@@ -19,4 +19,14 @@
       - Only use git commands when I explicitly tell you to do so.
     '';
   };
+  home.packages = [
+    (pkgs.writeScriptBin "codex" ''
+      #!/bin/sh
+
+      # According to my own bisect testing, this version is the last know version that is not affected by
+      # 1. This bug https://github.com/openai/codex/issues/1005
+      # 2. Does not explicitly require nodejs 22 to run.
+      ${pkgs.nodejs_22}/bin/npx -y @openai/codex@0.1.2505160811 "$@"
+    '')
+  ];
 }
