a1acff8c65bfd8556a77620cc7c4e50f591a2500
Louis Chan <louischan0325@gmail.com>
2025-05-23 02:24:52 +0800

    Use tab and shift-tab to navigate cells

--- .config/nvim/lua/lzn/vim-table-mode.lua
+++ .config/nvim/lua/lzn/vim-table-mode.lua
@@ -1,4 +1,8 @@
 require("lz.n").load({
   "vim-table-mode",
   event = { "DeferredUIEnter" },
+  before = function()
+    vim.g.table_mode_motion_right_map = "<Tab>"
+    vim.g.table_mode_motion_left_map = "<S-Tab>"
+  end,
 })
