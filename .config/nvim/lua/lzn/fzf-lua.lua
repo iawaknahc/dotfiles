6f07155fafa70e267a0e4256207879b3bd763348
Louis Chan <louischan0325@gmail.com>
2025-05-24 12:16:07 +0800

    Reconfigure fzf-lua
    1. Remove most keymaps that I do not actually use.
    2. Set up <Space><Space> to bring a Spotlight-like UX.
    3. Enable icons because I use Ghostty now. It supports icons by default.

--- .config/nvim/lua/lzn/fzf-lua.lua
+++ .config/nvim/lua/lzn/fzf-lua.lua
@@ -1,6 +1,8 @@
 require("lz.n").load({
   "fzf-lua",
   after = function()
+    local actions = require("fzf-lua").actions
+
     require("fzf-lua").setup({
       fzf_opts = {
         ["--layout"] = "default",
@@ -12,13 +14,19 @@ require("lz.n").load({
           delay = 10,
         },
       },
-      defaults = {
-        git_icons = false,
-        file_icons = false,
-      },
-      lsp = {
-        symbols = {
-          symbol_style = 3,
+      git = {
+        status = {
+          actions = {
+            -- By default left to stage, right to unstage.
+            ["left"] = false,
+            ["right"] = false,
+            -- Remove the keymap to discard changes in a file.
+            ["ctrl-x"] = false,
+            ["ctrl-s"] = {
+              fn = actions.git_stage_unstage,
+              reload = true,
+            },
+          },
         },
       },
     })
@@ -26,48 +34,31 @@ require("lz.n").load({
     require("fzf-lua").register_ui_select()
   end,
   cmd = "FzfLua",
+  -- Keep only the keymaps that I actually use.
+  -- For others, Use <Space><Space> to run :FzfLua, and then type the name to invoke the picker fuzzily.
   keys = {
+    {
+      "<Space><Space>",
+      "<CMD>FzfLua<CR>",
+      desc = "Open FzfLua",
+    },
+
     -- Inspired by Helix space mode f
     {
       "<Space>f",
       "<CMD>FzfLua git_files<CR>",
-      desc = "Open file",
+      desc = "Open files from project root",
+    },
+    {
+      "<Space>F",
+      "<CMD>FzfLua files<CR>",
+      desc = "Open files from working directory",
     },
     -- Inspired by Helix space mode b
     {
       "<Space>b",
       "<CMD>FzfLua buffers<CR>",
       desc = "Open buffers",
-    },
-    {
-      "<Space>:",
-      "<CMD>FzfLua commands<CR>",
-      desc = "Fuzzy search commands",
-    },
-    {
-      "<Space>j",
-      "<CMD>FzfLua jumps<CR>",
-      desc = "Pick from jumplist",
-    },
-    {
-      "<Space>g",
-      "<CMD>FzfLua live_grep<CR>",
-      desc = "Live grep in project",
-    },
-    {
-      "<Space>/",
-      "<CMD>FzfLua lgrep_curbuf<CR>",
-      desc = "Live grep in buffer",
-    },
-    {
-      "<Space>l",
-      "<CMD>FzfLua blines<CR>",
-      desc = "Fuzzy search lines",
-    },
-    {
-      "<Space>L",
-      "<CMD>FzfLua lines<CR>",
-      desc = "Fuzzy search all lines in open buffers",
     },
 
     -- https://gpanders.com/blog/whats-new-in-neovim-0-11/#more-default-mappings
@@ -82,30 +73,11 @@ require("lz.n").load({
       "<CMD>FzfLua lsp_references<CR>",
       desc = "Open references",
     },
-    -- c stands for incoming calls.
-    {
-      "<Space>c",
-      "<CMD>FzfLua lsp_incoming_calls<CR>",
-      desc = "Open incoming calls",
-    },
-    -- C stands for outgoing calls.
-    -- It is C because outgoing calls are less common.
-    {
-      "<Space>C",
-      "<CMD>FzfLua lsp_outgoing_calls<CR>",
-      desc = "Open outgoing calls",
-    },
-
     -- https://gpanders.com/blog/whats-new-in-neovim-0-11/#more-default-mappings
     {
       "gO",
       "<CMD>FzfLua lsp_document_symbols<CR>",
       desc = "Open symbols",
-    },
-    {
-      "<Space>S",
-      "<CMD>FzfLua lsp_live_workspace_symbols<CR>",
-      desc = "Open live symbols in workspace",
     },
 
     -- Diagnostics is preferred over loclist because it supports severity.
@@ -116,6 +88,7 @@ require("lz.n").load({
       "<CMD>FzfLua diagnostics_document<CR>",
       desc = "Open diagnostics",
     },
+    -- Inspired by Helix space mode D
     {
       "<Space>D",
       "<CMD>FzfLua diagnostics_workspace<CR>",
