754090c032b70cb174ffd365b02542dec8c5b6a6
Louis Chan <louischan0325@gmail.com>
2025-05-27 00:51:52 +0800

    Install the nerd font variant of JetBrains Mono

--- home-manager/fonts.nix
+++ home-manager/fonts.nix
@@ -1,7 +1,11 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    # This one is recognized as fixed-width.
+    # JetBrains Mono can be recognized as fixed-width by macOS Font Book.
+    #
+    # Install the nert-font variant of it so that I can use set it as
+    # the default monospace in Firefox to show nert font icons.
+    (with nerd-fonts; jetbrains-mono)
     jetbrains-mono
 
     # CJK fonts
