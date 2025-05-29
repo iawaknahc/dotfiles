5d7c4ac42fe0083d7b4abf43e2ccfda689764e91
Louis Chan <louischan0325@gmail.com>
2025-05-30 01:49:38 +0800

    Disable builtin history of shells

--- home-manager/atuin.nix
+++ home-manager/atuin.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, ... }:
 {
   programs.atuin.enable = true;
   programs.atuin.daemon.enable = false;
@@ -8,9 +8,28 @@
     # https://docs.atuin.sh/configuration/key-binding/#disable-up-arrow
     "--disable-up-arrow"
   ];
+
   programs.atuin.enableBashIntegration = true;
+  programs.bash.bashrcExtra = ''
+    unset HISTFILE
+  '';
+
   programs.atuin.enableZshIntegration = true;
+  programs.zsh.initContent = ''
+    unset HISTFILE
+  '';
+
   programs.atuin.enableFishIntegration = true;
+  # https://fishshell.com/docs/current/interactive.html#searchable-command-history
+  # https://fishshell.com/docs/current/interactive.html#private-mode
+  # Atuin respects fish_private_mode, when fish_private_mode is on, Atuin does not save history.
+  # Other than fish_private_mode, fish shell does not provide a way to disable history.
+  # Therefore, we forcibly make the history a symlink to /dev/null.
+  # fish shell will occashionally complain that it cannot write to, or change the permission of that file.
+  # Other than that, fish shell seems running fine.
+  xdg.dataFile."fish/fish_history".source = config.lib.file.mkOutOfStoreSymlink "/dev/null";
+
+  # nushell does not seem to have a way to disable history at all.
   programs.atuin.enableNushellIntegration = true;
 
   programs.atuin.settings = {
