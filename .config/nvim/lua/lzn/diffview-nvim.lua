d0c30c73998055da8735f79356704d8e11c435e5
Louis Chan <louischan0325@gmail.com>
2025-06-03 01:46:41 +0800

    Eagerly load diffview.nvim

--- .config/nvim/lua/lzn/diffview-nvim.lua
+++ .config/nvim/lua/lzn/diffview-nvim.lua
@@ -1,7 +1,8 @@
 require("lz.n").load({
   "diffview.nvim",
   enabled = vim.g.pager_enabled ~= 1,
-  event = { "DeferredUIEnter" },
+  -- Eagerly load it so that `nvim +DiffviewFileHistory` works as expected.
+  lazy = false,
   after = function()
     require("diffview").setup({})
   end,
