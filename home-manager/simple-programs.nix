7d768117e48f769a611bf87059d3238ad1f3da56
Louis Chan <louischan0325@gmail.com>
2025-06-05 01:05:28 +0800

    Install man-pages

--- home-manager/simple-programs.nix
+++ home-manager/simple-programs.nix
@@ -34,6 +34,31 @@
 
     jq
 
+    # The man program itself.
+    man
+    # The Linux man pages.
+    # The entries may be irrelevant to macOS.
+    man-pages
+    # The POSIX man pages, in particular,
+    # the package offers these sections:
+    # - 0p: header files (like stdio.h)
+    # - 1p: utilities (like sh, awk)
+    # - 3p: library functions (like strftime)
+    #
+    # If you want to access the man page of POSIX sh, you run
+    #   man 1p sh
+    #
+    # In contrast, running
+    #   man sh
+    # will likely take you to /usr/share/man/man1/sh.1 on macOS.
+    #
+    # To list all available variants, run
+    #   man -aw sh
+    # On my system, it prints
+    #   /usr/share/man/man1/sh.1
+    #   /Users/louischan/.nix-profile/share/man/man1p/sh.1p.gz
+    man-pages-posix
+
     # Metadata anonymisation toolkit 2
     # https://0xacab.org/jvoisin/mat2
     mat2
