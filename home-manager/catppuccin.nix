448ed7b7d81f04dc32d1cf0429f83d080876e760
Louis Chan <louischan0325@gmail.com>
2025-06-04 22:44:55 +0800

    Enable catppuccin for tmux

--- home-manager/catppuccin.nix
+++ home-manager/catppuccin.nix
@@ -8,6 +8,11 @@
   catppuccin.alacritty.enable = true;
   catppuccin.ghostty.enable = true;
   catppuccin.kitty.enable = true;
+  catppuccin.tmux.enable = true;
+  catppuccin.tmux.extraConfig = ''
+    set -g @catppuccin_status_background "default"
+    set -g @catppuccin_window_status_style "rounded"
+  '';
   catppuccin.fish.enable = true;
   catppuccin.atuin.enable = true;
   # wezterm has catppuccin bundled so we need not enable it here.
