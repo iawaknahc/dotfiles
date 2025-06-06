0366345dab94916764cf5fe026282ee673e419ae
Louis Chan <louischan@oursky.com>
2025-06-06 15:00:33 +0800

    Eagerly load vim-table-mode

--- .config/nvim/lua/lzn/vim-table-mode.lua
+++ .config/nvim/lua/lzn/vim-table-mode.lua
@@ -1,7 +1,9 @@
 require("lz.n").load({
   "vim-table-mode",
   enabled = vim.g.pager_enabled ~= 1,
-  event = { "DeferredUIEnter" },
+  -- If this plugin is loaded too late, its filetype plugin is not run.
+  -- One observable consequence is that b:table_mode_corner is not set for markdown.
+  lazy = false,
   before = function()
     vim.g.table_mode_motion_right_map = "<Tab>"
     vim.g.table_mode_motion_left_map = "<S-Tab>"
