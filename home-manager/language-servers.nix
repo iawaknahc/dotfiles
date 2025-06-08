11bc278722704d97cdb3615e338a3103e25efd13
Louis Chan <louischan0325@gmail.com>
2025-06-08 17:54:43 +0800

    Remove the workaround of tailwindcss-language-server

--- home-manager/language-servers.nix
+++ home-manager/language-servers.nix
@@ -1,30 +1,25 @@
 { pkgs, ... }:
 {
-  imports = [
-    ./tailwindcss-language-server.nix
+  home.packages = with pkgs; [
+    awk-language-server
+    bash-language-server
+    fish-lsp
+    gopls
+
+    # graphql-language-service-cli is installed with npx
+
+    marksman
+    # nil the the language server for Nix.
+    # See https://github.com/oxalica/nil
+    nil
+    nixd
+    pyright
+    sqls
+    tailwindcss-language-server
+    taplo
+    typescript-language-server
+    typos-lsp
+    vscode-langservers-extracted
+    yaml-language-server
   ];
-
-  config = {
-    home.packages = with pkgs; [
-      awk-language-server
-      bash-language-server
-      fish-lsp
-      gopls
-
-      # graphql-language-service-cli is installed with npx
-
-      marksman
-      # nil the the language server for Nix.
-      # See https://github.com/oxalica/nil
-      nil
-      nixd
-      pyright
-      sqls
-      taplo
-      typescript-language-server
-      typos-lsp
-      vscode-langservers-extracted
-      yaml-language-server
-    ];
-  };
 }
--- home-manager/tailwindcss-language-server.nix
+++ home-manager/tailwindcss-language-server.nix
@@ -1,16 +1,6 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    # It is observed that 0.0.27 tailwindcss-language-server is not an executable.
-    # Let's fix that ourselves.
-    # The upstream fix is https://github.com/NixOS/nixpkgs/commit/e4aacf43da9841ca12c530901dc8b45dc1235629
-    # but it is on master only.
-    (tailwindcss-language-server.overrideAttrs (prev: {
-      postInstall =
-        (prev.postInstall or "")
-        + ''
-          chmod u+x $out/lib/tailwindcss-language-server/packages/tailwindcss-language-server/bin/tailwindcss-language-server
-        '';
-    }))
+    tailwindcss-language-server
   ];
 }
