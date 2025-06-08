1a0b0a23df3cc17f9f2f9323d4e5b755cf8a5fe4
Louis Chan <louischan0325@gmail.com>
2025-06-08 15:14:13 +0800

    Remove mcp-server-time

--- home-manager/claude.nix
+++ home-manager/claude.nix
@@ -2,9 +2,6 @@
 {
   home.packages = with pkgs; [
     # From the overlay of natsukium/mcp-servers-nix
-    mcp-server-time
-
-    # From the overlay of natsukium/mcp-servers-nix
     #
     # As of 2025-06-08, Playwright supports full page screenshot out-of-the-box.
     # https://playwright.dev/docs/screenshots#full-page-screenshots
@@ -13,23 +10,39 @@
     playwright-mcp
   ];
 
+  home.file.".claude/CLAUDE.md".text = ''
+    # CLAUDE.md
+
+    ## Use Bash tool
+
+    Try using your Bash tool when you lack the ability to do something relevant to your task.
+
+    Here are some examples:
+
+    - In a Python script, you gain access to a cryptographically secure RNG.
+    - In a Python script, you gain access to datetime utility and calendar manipulation.
+
+    ## Date format
+
+    Unless otherwise specified, output date and time in ISO 8601 format.
+
+    Examples:
+    - `2006-01-02`
+    - `2006-01-02T15:04:05Z`
+    - `2006-01-02T15:04:05.999+07:00`
+  '';
+
   # https://modelcontextprotocol.io/quickstart/user#2-add-the-filesystem-mcp-server
   home.file."Library/Application Support/Claude/claude_desktop_config.json" = {
     text = builtins.toJSON {
       mcpServers = {
-        time = {
-          command = "${config.home.profileDirectory}/bin/mcp-server-time";
-          args = [
-            # For some unknown reason, the timezone name it detects is HKT,
-            # which is not a valid entry in the tz database.
-            # Therefore we provide an accurate one.
-            "--local-timezone"
-            "Asia/Hong_Kong"
-          ];
-        };
         playwright = {
           command = "${config.home.profileDirectory}/bin/mcp-server-playwright";
         };
+
+        # mcp-server-time is very limited.
+        # On Claude Code, it is better ask to the Bash tool to do time related manipulation.
+        # On Claude Desktop, it has no Bash tool, so it cannot do similar thing.
 
         # No need to install mcp-server-fetch because Claude Desktop and Claude Code has it built in.
       };
