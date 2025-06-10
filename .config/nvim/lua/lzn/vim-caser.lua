72d7494caae7e0a3efb34b647e11c5d3d4b7ddfe
Louis Chan <louischan0325@gmail.com>
2025-06-10 23:16:12 +0800

    Make vim-caser work with visual selection

--- .config/nvim/lua/lzn/vim-caser.lua
+++ .config/nvim/lua/lzn/vim-caser.lua
@@ -19,16 +19,22 @@ require("lz.n").load({
 
     for _, variant in ipairs(variants) do
       local command_name, caser_name, desc = unpack(variant)
-      vim.api.nvim_create_user_command("To" .. command_name, function()
-        vim.fn["caser#ActionSetup"](caser_name)
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("g@", true, false, true), "n", false)
-        -- I failed to get this work. So the command does not work for visual mode.
-        -- local mode = vim.api.nvim_get_mode()
-        -- if mode == "v" then
-        --   vim.fn["caser#DoAction"](caser_name, vim.fn.visualmode())
-        -- end
+      vim.api.nvim_create_user_command("To" .. command_name, function(args)
+        -- When the user command is created with range=true,
+        -- The range field is
+        -- 0 when the user command is executed without a visual selection.
+        -- 2 when the user command is executed with a visual selection.
+        local range = args.range
+
+        if range == 0 then
+          vim.fn["caser#ActionSetup"](caser_name)
+          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("g@", true, false, true), "n", false)
+        elseif range == 2 then
+          vim.fn["caser#DoAction"](caser_name, vim.fn.visualmode())
+        end
       end, {
         desc = desc,
+        range = true,
       })
     end
   end,
