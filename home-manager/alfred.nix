4c5b8231f5c757f570e6fa795b7754d90a8bda94
Louis Chan <louischan0325@gmail.com>
2025-06-12 21:51:14 +0800

    Install cur

--- home-manager/alfred.nix
+++ home-manager/alfred.nix
@@ -29,4 +29,31 @@
         '';
       };
     };
+
+  xdg.configFile."alfred/Alfred.alfredpreferences/workflows/user.workflow.00000000-0000-0000-00000000000000002" =
+    {
+      recursive = true;
+      source = pkgs.stdenv.mkDerivation rec {
+        pname = "alfred-workflow-cur";
+        version = "2025.2";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "alfredapp";
+          repo = "currency-converter-workflow";
+          rev = "${version}";
+          hash = "sha256-142e1KZ58+cEBxJGq/W8JElemGgXTHEP0PTbsOV11F0=";
+        };
+
+        buildPhase = ''
+          # It is observed that Alfred will actually change this file when configuration of this workflow is changed in the GUI.
+          # Thus remove this file from the derivation and manage it with chezmoi.
+          rm ./Workflow/info.plist
+        '';
+
+        installPhase = ''
+          mkdir $out
+          cp -R ./Workflow/. $out/
+        '';
+      };
+    };
 }
