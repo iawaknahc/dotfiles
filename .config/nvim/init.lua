f0c25e68e90a05c8907479cd800fde5bdfa7a1fc
Louis Chan <louischan0325@gmail.com>
2025-06-14 23:20:23 +0800

    Use my own implementation of gf

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -150,6 +150,32 @@ vim.keymap.set({ "i", "c" }, [[<C-\><C-p>]], "<C-r>=getcwd()<CR>", {
 vim.keymap.set({ "i", "c" }, [[<C-\><C-a>]], [[<C-r>=expand("%:p")<CR>]], {
   desc = "Insert absolute path to current file",
 })
+vim.keymap.set("n", "gf", function()
+  -- I found that both vim and neovim does not handle the case that
+  -- the file under cursor does not exist yet.
+  --
+  -- In :help gf, it is suggested that you can
+  -- :map gf :edit <cfile><CR>
+  -- But that does not seem to handle path like ../.foo/bar
+
+  local file_under_cursor = vim.fn.expand("<cfile>")
+
+  -- Absolute path
+  if vim.startswith(file_under_cursor, "/") then
+    -- Fallback to original gf.
+    vim.cmd([[normal! gf]])
+    return
+  end
+
+  -- Otherwise we construct a path relative to the current file.
+  local parent_directory = vim.fn.expand("%:h")
+  local path = vim.fs.joinpath(parent_directory, file_under_cursor)
+  local normalized = vim.fs.normalize(path)
+  local fname = vim.fn.fnameescape(normalized)
+  vim.cmd([[edit ]] .. fname)
+end, {
+  desc = "Edit file under cursor",
+})
 vim.keymap.set({ "n" }, "gx", function()
   local function gx()
     local word = vim.fn.expand("<cfile>")
