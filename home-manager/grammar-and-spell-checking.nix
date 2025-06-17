6b44fd77be1295c3788de0843589945d02f7d8a2
Louis Chan <louischan0325@gmail.com>
2025-06-18 02:05:46 +0800

    Document my experience on LanguageTool

--- home-manager/grammar-and-spell-checking.nix
+++ home-manager/grammar-and-spell-checking.nix
@@ -3,8 +3,15 @@
   home.packages = with pkgs; [
     harper
     typos-lsp
+
     # Without dictionary, cspell produces many false positives.
     # nodePackages.cspell
+
     codespell
+
+    # The following tools are not very practical.
+    # They consume 1GB, even without the n-gram data.
+    # languagetool
+    # ltex-ls-plus
   ];
 }
