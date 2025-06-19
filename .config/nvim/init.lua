97592962971a23d241d0479327e367e5b077c665
Louis Chan <louischan0325@gmail.com>
2025-06-19 21:41:37 +0800

    Set scrolloff=15

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -67,6 +67,14 @@ vim.o.cursorline = true
 -- Completion
 vim.opt.completeopt = { "menu", "menuone", "noselect" }
 vim.o.pumheight = 10
+-- The completion menu of common completion plugins (e.g. blink.cmp) has a default height of 10.
+-- We set scrolloff to 15 so that we can see the completion menu plus a context of 5 lines.
+-- Note that scrolloff does not work at the end of the buffer.
+-- See https://github.com/vim/vim/issues/13428
+-- There is a plugin https://github.com/Aasim-A/scrollEOF.nvim
+-- I tried that plugin but that plugin will interfere other plugins that create it own filetype, such as fzf-lua.
+-- It is just too cumbersome to exclude those plugin-private filetypes.
+vim.o.scrolloff = 15
 
 -- Command mode
 -- shell by default is $SHELL.
@@ -82,9 +90,6 @@ vim.o.swapfile = false
 -- This also controls how often vim-gitgutter updates the signs.
 -- :h updatetime
 vim.o.updatetime = 100
-
--- Auto scrolling
-vim.o.scrolloff = 5
 
 -- clipboard
 vim.opt.clipboard:append({ "unnamed" })
