97cfa86b533d4ba35454bc9be30aacefe6181fbd
Louis Chan <louischan0325@gmail.com>
2025-06-21 02:22:41 +0800

    Abbreviate grep -> Grep

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -66,6 +66,9 @@
     command! -nargs=+ -complete=file_in_path -bar Grep  cgetexpr Grep(<f-args>)
     command! -nargs=+ -complete=file_in_path -bar LGrep lgetexpr Grep(<f-args>)
 
+    cnoreabbrev <expr> grep  (getcmdtype() ==# ':' && getcmdline() ==# 'grep')  ? 'Grep'  : 'grep'
+    cnoreabbrev <expr> lgrep (getcmdtype() ==# ':' && getcmdline() ==# 'lgrep') ? 'LGrep' : 'lgrep'
+
     augroup quickfix
       autocmd!
       autocmd QuickFixCmdPost cgetexpr cwindow
