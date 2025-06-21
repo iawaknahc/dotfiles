8cb9d5bbab9ce6385af8d4e14329649ef6dfe340
Louis Chan <louischan0325@gmail.com>
2025-06-21 13:31:12 +0800

    Make treewalker not crash anymore

--- .config/nvim/lua/lzn/treewalker-nvim.lua
+++ .config/nvim/lua/lzn/treewalker-nvim.lua
@@ -9,17 +9,42 @@ require("lz.n").load({
       highlight_group = "IncSearch",
     })
 
+    -- As of 2025-06-21, https://github.com/aaronik/treewalker.nvim/commits/cfae49dedd041dbe867c2b3d0b081fc381a735e9/
+    -- This plugin assumes that parser:parse() has been called once.
+    -- This may not be true if I do not have other plugin doing that.
+    -- So we have quick workaround by calling parser:parse(true) before letting treewalker to do the actual work.
+    local function fix(fn)
+      return function(...)
+        local ok, parser = pcall(vim.treesitter.get_parser)
+        if ok and parser ~= nil then
+          parser:parse(true)
+        end
+        fn(...)
+      end
+    end
+
+    local fixed_treewalker = {
+      move_up = fix(treewalker.move_up),
+      move_in = fix(treewalker.move_in),
+      move_down = fix(treewalker.move_down),
+      move_out = fix(treewalker.move_out),
+      swap_up = fix(treewalker.swap_up),
+      swap_right = fix(treewalker.swap_right),
+      swap_down = fix(treewalker.swap_down),
+      swap_left = fix(treewalker.swap_left),
+    }
+
     vim.keymap.set({ "n", "x" }, "<M-k>", function()
-      treewalker.move_up()
+      fixed_treewalker.move_up()
     end, { desc = "Treewalker: Move up" })
     vim.keymap.set({ "n", "x" }, "<M-l>", function()
-      treewalker.move_in()
+      fixed_treewalker.move_in()
     end, { desc = "Treewalker: Move in" })
     vim.keymap.set({ "n", "x" }, "<M-j>", function()
-      treewalker.move_down()
+      fixed_treewalker.move_down()
     end, { desc = "Treewalker: Move down" })
     vim.keymap.set({ "n", "x" }, "<M-h>", function()
-      treewalker.move_out()
+      fixed_treewalker.move_out()
     end, { desc = "Treewalker: Move out" })
 
     -- This is a very simplified version of dot-repeat.
@@ -39,7 +64,7 @@ require("lz.n").load({
           -- l is just a placeholder motion to trigger g@ to execute once.
           return "g@l"
         end
-        treewalker[fn_name]()
+        fixed_treewalker[fn_name]()
       end
       return _G[global_fn_name]
     end
