651004d7048a76a5d9f2ae9cdadc6a6f8d985890
Louis Chan <louischan0325@gmail.com>
2025-06-28 20:59:26 +0800

    Install coloraide

--- home-manager/python.nix
+++ home-manager/python.nix
@@ -4,35 +4,53 @@
 }:
 let
   # python3 is usually one version behind.
-  version = "313";
+  pythonVersion = "313";
 in
 {
   nixpkgs.overlays = [
-    (final: prev: {
-      mypython = prev."python${version}".withPackages (
-        python-pkgs: with python-pkgs; [
-          # Timezone handling
-          tzdata
-          pytz
-          tzlocal
-          # Clipboard
-          pyperclip
-          # Parse tiny language
-          parsy
-          # Terminal output
-          rich
-          # Manipulating fonts
-          fonttools
-        ]
-      );
-    })
+    (
+      final: prev:
+      let
+        prevPython = prev."python${pythonVersion}";
+        prevPythonPackages = prev."python${pythonVersion}Packages";
+      in
+      {
+        mypython = prevPython.withPackages (
+          python-pkgs: with python-pkgs; [
+            # Timezone handling
+            tzdata
+            pytz
+            tzlocal
+            # Clipboard
+            pyperclip
+            # Parse tiny language
+            parsy
+            # Terminal output
+            rich
+            # Manipulating colors
+            (prevPythonPackages.buildPythonPackage rec {
+              pname = "coloraide";
+              version = "4.7.2";
+              pyproject = true;
+              build-system = [ prevPythonPackages.hatchling ];
+              src = prev.fetchPypi {
+                inherit pname version;
+                hash = "sha256-fomOKtF3hzgJvR9f2x2QYYrYdASf6tlS/0Rw0VdmbUs=";
+              };
+            })
+            # Manipulating fonts
+            fonttools
+          ]
+        );
+      }
+    )
   ];
 
   home.packages = [
     pkgs.mypython
     pkgs.ruff
     pkgs.pyright
-    pkgs."python${version}Packages".json5
-    pkgs."python${version}Packages".debugpy
+    pkgs."python${pythonVersion}Packages".json5
+    pkgs."python${pythonVersion}Packages".debugpy
   ];
 }
