53eeea2f4c38995cf578bbdc258222d7e2aca4e6
Louis Chan <louischan0325@gmail.com>
2025-07-10 00:59:31 +0800

    Install cldr-json

--- home-manager/unicode.nix
+++ home-manager/unicode.nix
@@ -1,6 +1,20 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
+    (stdenvNoCC.mkDerivation rec {
+      pname = "cldr-json";
+      version = "47.0.0";
+      src = fetchzip {
+        url = "https://github.com/unicode-org/cldr-json/releases/download/${version}/cldr-${version}-json-full.zip";
+        stripRoot = false;
+        hash = "sha256-+xELEHy3hq2botBV6buouPuKMk8qWnokTsnV2h57jbQ=";
+      };
+      installPhase = ''
+        mkdir -p $out/share/cldr-json
+        mv cldr-* $out/share/cldr-json/
+      '';
+    })
+
     unicode-character-database
     # This program is known to be broken on nixpkgs
     # See https://github.com/NixOS/nixpkgs/pull/420887
