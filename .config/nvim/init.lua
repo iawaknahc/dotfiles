9b299e1e86a8679d7498a12976de16e6e893c035
Louis Chan <louischan0325@gmail.com>
2025-07-17 23:21:24 +0800

    Add :Reverse command

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -479,6 +479,17 @@ vim.keymap.set("n", "<M-/>", ":Lvimgrep<Space>", {
   desc = "/ to loclist",
 })
 
+vim.api.nvim_create_user_command("Reverse", function(args)
+  local line1 = args.line1
+  local line2 = args.line2
+  local command = string.format("%d,%dglobal/^/move %d", line1, line2, line1 - 1)
+  vim.cmd(command)
+  vim.notify(command)
+end, {
+  desc = "Reverse line order as in :help 12.4",
+  range = "%",
+})
+
 -- Diagnostic
 vim.diagnostic.config({
   virtual_text = {
