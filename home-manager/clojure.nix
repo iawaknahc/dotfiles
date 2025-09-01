075a6884cc5f9608aa661ebfacb1996fa5960e6f
Louis Chan <louischan0325@gmail.com>
2025-09-01 23:00:08 +0800

    Make icu4j available in clj

--- home-manager/clojure.nix
+++ home-manager/clojure.nix
@@ -8,4 +8,13 @@
     boot
     leiningen
   ];
+
+  # https://clojure.org/reference/clojure_cli#config_dir
+  home.file.".clojure/deps.edn".text = ''
+    {
+      :deps {
+        com.ibm.icu/icu4j {:mvn/version "77.1"}
+      }
+    }
+  '';
 }
