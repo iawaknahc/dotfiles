f0c123e9d956a468ce66f87e417e41faf481cd95
Louis Chan <louischan0325@gmail.com>
2025-11-17 00:13:59 +0800

    Remove downloadURL

--- home-manager/firefox/violentmonkey/force-wikipedia-in-zh-hk.user.js
+++ home-manager/firefox/violentmonkey/force-wikipedia-in-zh-hk.user.js
@@ -3,7 +3,6 @@
 // @version     0.1.0
 // @match       https://zh.wikipedia.org/*
 // @run-at      document-start
-// @downloadURL https://raw.githubusercontent.com/iawaknahc/dotfiles/refs/heads/master/violentmonkey/force-wikipedia-in-zh-hk.user.js
 // ==/UserScript==
 
 (() => {
