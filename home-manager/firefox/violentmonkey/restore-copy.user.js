d43539b3049e5380e6959ca95118cb94c03d5842
Louis Chan <louischan0325@gmail.com>
2025-12-10 14:18:12 +0800

    Unbreak copy on docs.google.com

--- home-manager/firefox/violentmonkey/restore-copy.user.js
+++ home-manager/firefox/violentmonkey/restore-copy.user.js
@@ -1,8 +1,11 @@
 // ==UserScript==
-// @name        Restore copy on annoying websites
-// @match       https://*/*
-// @run-at      document-start
+// @name          Restore copy on annoying websites
+// @match         https://*/*
+// @exclude-match https://docs.google.com/*
+// @run-at        document-start
 // ==/UserScript==
+
+// We have to exclude docs.google.com, otherwise copy is broken in Google Docs.
 
 (() => {
   function copy(e) {
