454e0f7db9918f6c4171913ad43f5e90f3ef50f3
Louis Chan <louischan0325@gmail.com>
2026-02-04 19:42:48 +0800

    Fix and harden the configuration of Syncthing

--- home-manager/syncthing.nix
+++ home-manager/syncthing.nix
@@ -1,20 +1,47 @@
 {
-  config,
   ...
 }:
 {
   services.syncthing.enable = true;
+  services.syncthing.overrideDevices = true;
+  services.syncthing.overrideFolders = true;
 
-  # FIXME: It seems that the config I declare here is not applied to the service at all.
-
-  sops.secrets."syncthing/devices/LouisChan16/id" = { };
+  # It is impossible to use secrets at evaluation time of nix code.
+  # https://github.com/Mic92/sops-nix?tab=readme-ov-file#using-secrets-at-evaluation-time
   services.syncthing.settings.devices."LouisChan16" = {
-    id = config.sops.placeholder."syncthing/devices/LouisChan16/id";
+    id = "DZH4SZM-YXPC5JN-3KCTXKQ-MRCJ5QM-STPPXNN-ZY55RQG-YRIUWHV-NBXKNQ5";
   };
 
   services.syncthing.settings.folders."~/personal/" = {
     id = "obsidian-personal";
     type = "sendonly";
     devices = [ "LouisChan16" ];
+  };
+
+  # According to https://github.com/nix-community/home-manager/blob/master/modules/services/syncthing.nix
+  # ~/Library/Application Support/Syncthing/config.xml IS NOT modified at all.
+  # Instead, the config is modified via the JSON REST API.
+  # However, according to my observation, I need to manually restart Syncthing in the GUI to apply the configuration.
+  # FIXME: Actually this is a bug https://github.com/nix-community/home-manager/issues/6542
+  # There is a PR too https://github.com/nix-community/home-manager/pull/8415
+  services.syncthing.settings.options = {
+    listenAddresses = [ "tcp://0.0.0.0:22000" ];
+
+    globalAnnounceServers = [ ];
+    stunServers = [ ];
+    urURL = "https://0.0.0.0";
+    releasesURL = "https://0.0.0.0";
+    crURL = "https://0.0.0.0";
+
+    globalAnnounceEnabled = false;
+    localAnnounceEnabled = false;
+    crashReportingEnabled = false;
+    relaysEnabled = false;
+    natEnabled = false;
+    announceLANAddresses = false;
+    startBrowser = false;
+    urAccepted = -1;
+
+    autoUpgradeIntervalH = 0;
   };
 }
