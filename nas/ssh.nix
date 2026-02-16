8d8c131584f57634ce6188ad7ae332aaf086ff34
Louis Chan <louischan0325@gmail.com>
2026-02-17 02:14:41 +0800

    Harden sshd config

--- nas/ssh.nix
+++ nas/ssh.nix
@@ -2,6 +2,9 @@
 {
   services.openssh.enable = true;
   services.openssh.settings = {
+    PasswordAuthentication = false;
+    KbdInteractiveAuthentication = false;
+    PermitRootLogin = "no";
     AcceptEnv = [ "LS_COLORS" ];
   };
   # By default, these are true, causing LS_COLORS set by SSH client to be overridden.
