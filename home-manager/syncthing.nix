b6cc051e76d3f7f9914a3592cd025ecdb1e1206d
Louis Chan <louischan0325@gmail.com>
2026-02-16 13:25:05 +0800

    Sync to NAS only

--- home-manager/syncthing.nix
+++ home-manager/syncthing.nix
@@ -8,12 +8,6 @@
 
   # It is impossible to use secrets at evaluation time of nix code.
   # https://github.com/Mic92/sops-nix?tab=readme-ov-file#using-secrets-at-evaluation-time
-  services.syncthing.settings.devices."LouisChan16" = {
-    id = "DZH4SZM-YXPC5JN-3KCTXKQ-MRCJ5QM-STPPXNN-ZY55RQG-YRIUWHV-NBXKNQ5";
-    addresses = [
-      "tcp://louischan-16-pro.tail78d407.ts.net:22000"
-    ];
-  };
   services.syncthing.settings.devices."nas" = {
     id = "AS5FKVU-YOJNR3P-FRSEUYG-BOY47VC-UB2ZJYK-EBJYV56-GEJCVAP-ABNHOAD";
     addresses = [
@@ -28,7 +22,6 @@
     id = "obsidian-personal";
     type = "sendreceive";
     devices = [
-      "LouisChan16"
       "nas"
     ];
   };
