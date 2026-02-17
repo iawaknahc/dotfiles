e2a541cda26a399fa24da32ab6a27f1103f37781
Louis Chan <louischan0325@gmail.com>
2026-02-17 21:16:19 +0800

    Simplify the setup of direnv

--- home-manager/direnv.nix
+++ home-manager/direnv.nix
@@ -4,41 +4,13 @@
   ...
 }:
 {
-  home.packages = [
-    config.programs.direnv.package
-    config.programs.direnv.nix-direnv.package
-  ];
-  xdg.configFile."direnv/lib/hm-nix-direnv.sh" = {
-    enable = true;
-    source = "${config.programs.direnv.nix-direnv.package}/share/nix-direnv/direnvrc";
-  };
-
-  programs.bash.initExtra = lib.mkAfter ''
-    eval "$(${config.programs.direnv.package}/bin/direnv hook bash)"
-  '';
-
+  programs.direnv.enable = true;
+  programs.direnv.nix-direnv.enable = true;
+  programs.direnv.enableBashIntegration = true;
+  programs.direnv.enableFishIntegration = true;
+  programs.direnv.enableNushellIntegration = true;
+  programs.direnv.enableZshIntegration = true;
   programs.x-elvish.rcExtra = lib.mkAfter ''
     eval (${config.programs.direnv.package}/bin/direnv hook elvish | slurp)
-  '';
-
-  programs.fish.interactiveShellInit = lib.mkAfter ''
-    ${config.programs.direnv.package}/bin/direnv hook fish | source
-  '';
-
-  programs.nushell.extraConfig = lib.mkAfter ''
-    $env.config = ($env.config | upsert hooks.env_change.PWD {|config|
-      let val = ($config | get -i hooks.env_change.PWD)
-      if $val == null {
-        $val | append {|| ${config.programs.direnv.package}/bin/direnv export json | from json | default {} | load-env }
-      } else {
-        [
-          {|| ${config.programs.direnv.package}/bin/direnv export json | from json | default {} | load-env }
-        ]
-      }
-    })
-  '';
-
-  programs.zsh.initContent = lib.mkAfter ''
-    eval "$(${config.programs.direnv.package}/bin/direnv hook zsh)"
   '';
 }
