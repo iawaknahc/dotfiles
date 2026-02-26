a36ba78724cb7fa191a3526b6194a753827e6884
Louis Chan <louischan0325@gmail.com>
2026-02-27 01:35:09 +0800

    Ignore more files

--- home-manager/fd.nix
+++ home-manager/fd.nix
@@ -3,7 +3,32 @@
   programs.fd.enable = true;
   programs.fd.hidden = true;
   programs.fd.ignores = [
-    # Ignore .git even -H is used.
+    # Git
     ".git/"
+
+    # NodeJS ecosystem
+    "node_modules/"
+
+    # macOS
+    "Library/"
+    "Volumes/"
+    "Applications/"
+
+    # A heuristic to ignore GOPATH
+    "go/"
+    # A heuristic to ignore a local copy of NixOS/nixpkgs
+    "nixpkgs/"
+
+    # Like ~/.android/cache ~/.bundle/cache
+    "cache/"
+    # Like ~/.gradle/caches
+    "caches/"
+
+    # XDG_CACHE_HOME
+    ".cache/"
+
+    ".local/share/android/"
+    ".rustup/toolchains/"
+    ".weasis/cache-*/"
   ];
 }
