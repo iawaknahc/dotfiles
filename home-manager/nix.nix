87e1a20b9e688bf2bc4470d5b2a5f300f4c3c1f1
Louis Chan <louischan0325@gmail.com>
2026-03-30 23:05:51 +0800

    Update nix-output-monitor to a snapshot newer than 2.1.8

--- home-manager/nix.nix
+++ home-manager/nix.nix
@@ -1,5 +1,21 @@
 { pkgs, ... }:
 {
+  # FIXME: Remove the overlay of nix-output-monitor when a newer release than 2.1.8 is out.
+  # As of 2026-03-30, the latest release 2.1.8 (released on 2025-11-09) of nix-output-monitor does not show download progress.
+  # The commit https://github.com/maralorn/nix-output-monitor/commit/0cb46615fb8187e4598feac4ccf8f27a06aae0b7 made on 2025-11-20 does that.
+  nixpkgs.overlays = [
+    (final: prev: {
+      nix-output-monitor = prev.nix-output-monitor.overrideAttrs {
+        src = prev.fetchFromGitHub {
+          owner = "maralorn";
+          repo = "nix-output-monitor";
+          rev = "4c34e115ab344df485316d4a61768b8d561fbeb3";
+          hash = "sha256-CcdGDNLkCsncYI+S5O71YgxQm2XLD8zPiDQQIebEdJ0=";
+        };
+      };
+    })
+  ];
+
   home.packages = with pkgs; [
     flake-checker
     nurl
