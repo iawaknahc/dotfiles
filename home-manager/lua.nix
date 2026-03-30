6ee5b702abbe4f1dc6881e065d8caca0c29675f1
Louis Chan <louischan0325@gmail.com>
2026-03-30 22:33:20 +0800

    Update flake.lock

--- home-manager/lua.nix
+++ home-manager/lua.nix
@@ -3,7 +3,7 @@
   ...
 }:
 let
-  lua = with pkgs; lua5_4;
+  lua54 = with pkgs; lua5_4;
 in
 {
   home.packages = with pkgs; [
@@ -15,9 +15,9 @@ in
     fnlfmt
 
     # Luarocks hits a limit of LuaJIT.
-    # So we have to run luarocks with Lua 5.4
+    # So we have to run luarocks with Lua > 5.1
     # https://github.com/luarocks/luarocks/issues/1797
-    (lua.withPackages (
+    (lua54.withPackages (
       packages: with packages; [
         luautf8
         fennel
@@ -33,18 +33,18 @@ in
         # sbarlua is the Lua API to SketchyBar
         (sbarlua.override {
           # By inspecting the source code
-          # https://github.com/NixOS/nixpkgs/blob/d8cf13dc00548190c399847ef8acb0640999d748/pkgs/by-name/sb/sbarlua/package.nix#L9
-          # We know that sbarlua is originally built with lua54Packages.buildLuaPackage
-          # So we want to override the package input lua54Packages.
+          # https://github.com/NixOS/nixpkgs/blob/nixpkgs-unstable/pkgs/by-name/sb/sbarlua/package.nix
+          # We know that sbarlua is originally built with lua55Packages.buildLuaPackage
+          # So we want to override the package input lua55Packages.
           #
           # With some study, we found out that the lua interpreter packages (such as lua5_4_compat) has an attribute `pkgs`,
-          # which is compatible with lua54Packages.
+          # which is compatible with lua55Packages.
           # See https://nixos.org/manual/nixpkgs/stable/#attributes-on-lua-interpreters-packages
           #
-          # So we can simply override lua54lua54Packages with <interpreter-package>.pkgs
+          # So we can simply override lua55Packages with <interpreter-package>.pkgs
           #
           # If we do not override, we will run into a problem of installing 2 conflicting copies of Lua interpreters.
-          lua54Packages = lua.pkgs;
+          lua55Packages = lua54.pkgs;
         })
       ]
     ))
