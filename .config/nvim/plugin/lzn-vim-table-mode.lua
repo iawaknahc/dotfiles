34e7c1bfdb957bdcd17a594f223c0b37f4083ccc
Louis Chan <louischan0325@gmail.com>
2026-04-01 03:02:17 +0800

    Do not micro-manage vim-table-mode

--- .config/nvim/plugin/lzn-vim-table-mode.lua
+++ .config/nvim/plugin/lzn-vim-table-mode.lua
@@ -1,10 +1,4 @@
-require("lz.n").load({
-  "vim-table-mode",
-  -- If this plugin is loaded too late, its filetype plugin is not run.
-  -- One observable consequence is that b:table_mode_corner is not set for markdown.
-  event = { "FileType" },
-  before = function()
-    vim.g.table_mode_motion_right_map = "<Tab>"
-    vim.g.table_mode_motion_left_map = "<S-Tab>"
-  end,
-})
+vim.g.table_mode_motion_right_map = "<Tab>"
+vim.g.table_mode_motion_left_map = "<S-Tab>"
+
+vim.cmd([[packadd vim-table-mode]])
