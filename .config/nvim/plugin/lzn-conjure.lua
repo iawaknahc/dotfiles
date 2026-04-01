01bd78525d83f8c0548b346036d3eab00b456ca1
Louis Chan <louischan0325@gmail.com>
2026-04-01 18:49:44 +0800

    Load conjure with :packadd

--- .config/nvim/plugin/lzn-conjure.lua
+++ .config/nvim/plugin/lzn-conjure.lua
@@ -1,12 +1,6 @@
-require("lz.n").load({
-  "conjure",
-  event = { "DeferredUIEnter" },
-  before = function()
-    vim.g["conjure#mapping#def_word"] = false
-    vim.g["conjure#mapping#doc_word"] = false
+vim.g["conjure#mapping#def_word"] = false
+vim.g["conjure#mapping#doc_word"] = false
+vim.g["conjure#completion#omnifunc"] = false
+vim.g["conjure#highlight#enabled"] = true
 
-    vim.g["conjure#completion#omnifunc"] = false
-
-    vim.g["conjure#highlight#enabled"] = true
-  end,
-})
+vim.cmd([[packadd conjure]])
