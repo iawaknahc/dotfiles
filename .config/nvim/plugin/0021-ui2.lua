774537443806b3821b67dd5e9fe85be31f9b28e9
Louis Chan <louischan0325@gmail.com>
2026-04-02 21:04:56 +0800

    Tweak the configuration of ui2

--- .config/nvim/plugin/0021-ui2.lua
+++ .config/nvim/plugin/0021-ui2.lua
@@ -1,6 +1,18 @@
 require("vim._core.ui2").enable({
   enable = true,
   msg = {
-    targets = "msg",
+    -- There are 3 targets:
+    -- cmd: Shown in the cmd.
+    -- msg: Shown in the self-dismissing popup.
+    -- pager: It is :messages.
+    --
+    -- If the target is "pager", then the pager is focused, which may be annoying. Therefore, "pager" is never used.
+    --
+    -- Use g< to view recent messages
+    target = "cmd",
+    targets = {
+      -- LSP notification messages should be shown in self-dismissing popups.
+      progress = "msg",
+    },
   },
 })
