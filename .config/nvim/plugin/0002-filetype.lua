f40858c974521d837e9796be417de8c489984b4f
Louis Chan <louischan0325@gmail.com>
2026-04-06 03:38:17 +0800

    Use vim.filetype.add to define extra filetypes

--- .config/nvim/plugin/0002-filetype.lua
+++ .config/nvim/plugin/0002-filetype.lua
@@ -1,20 +1,12 @@
-local myfiletype_autocmdgroup = vim.api.nvim_create_augroup("MyFiletype", { clear = true })
-
-local function myautocmd_set_filetype(pattern, filetype)
-  vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-    group = myfiletype_autocmdgroup,
-    pattern = pattern,
-    callback = function()
-      vim.bo.filetype = filetype
-    end,
-  })
-end
-
--- By default, filetype.vim treats *.env as sh
--- We do NOT want to run any before-save fix on *.env
--- For example, some envvars may have trailing whitespaces we do want to preserve.
-myautocmd_set_filetype("*.env", "")
--- mjml
-myautocmd_set_filetype("*.mjml", "html")
-myautocmd_set_filetype("docker-compose.yaml", "yaml.docker-compose")
-myautocmd_set_filetype("docker-compose.yml", "yaml.docker-compose")
+-- If you run :checkhealth, we may still see a warning saying yaml.docker-compose is not a valid filetype.
+-- For a filetype to be considered as valid, there have to be a ftplugin/<filetype>.{vim,lua}
+-- But it is really a warning.
+vim.filetype.add({
+  extension = {
+    mjml = "html",
+  },
+  filename = {
+    ["docker-compose.yaml"] = "yaml.docker-compose",
+    ["docker-compose.yml"] = "yaml.docker-compose",
+  },
+})
