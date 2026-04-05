03f68ad7e06e71045fc7bfc5b138c617eefc7775
Louis Chan <louischan0325@gmail.com>
2026-04-05 17:51:45 +0800

    Formally configure Syncthing devices

--- home-manager/syncthing.nix
+++ home-manager/syncthing.nix
@@ -14,6 +14,12 @@
       "tcp://nas.tail78d407.ts.net:22000"
     ];
   };
+  services.syncthing.settings.devices."LouisChan16" = {
+    id = "DZH4SZM-YXPC5JN-3KCTXKQ-MRCJ5QM-STPPXNN-ZY55RQG-YRIUWHV-NBXKNQ5";
+    addresses = [
+      "tcp://louischan-16-pro.tail78d407.ts.net:22000"
+    ];
+  };
 
   # This folder is assumed to contain a file ".stignore" at the root.
   # The content of .stignore should be
@@ -23,15 +29,10 @@
     type = "sendreceive";
     devices = [
       "nas"
+      "LouisChan16"
     ];
   };
 
-  # According to https://github.com/nix-community/home-manager/blob/master/modules/services/syncthing.nix
-  # ~/Library/Application Support/Syncthing/config.xml IS NOT modified at all.
-  # Instead, the config is modified via the JSON REST API.
-  # However, according to my observation, I need to manually restart Syncthing in the GUI to apply the configuration.
-  # FIXME: Actually this is a bug https://github.com/nix-community/home-manager/issues/6542
-  # There is a PR too https://github.com/nix-community/home-manager/pull/8415
   services.syncthing.settings.options = {
     listenAddresses = [ "tcp://0.0.0.0:22000" ];
 
