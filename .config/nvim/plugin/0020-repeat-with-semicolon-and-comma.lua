6a07c296cd330726e17b6be47e2944340b862b55
Louis Chan <louischan0325@gmail.com>
2026-04-05 16:02:44 +0800

    Make ; and , work with v_an and v_in

--- .config/nvim/plugin/0020-repeat-with-semicolon-and-comma.lua
+++ .config/nvim/plugin/0020-repeat-with-semicolon-and-comma.lua
@@ -1,129 +1,135 @@
-local lh = { forward = "l", backward = "h" }
-local jk = { forward = "j", backward = "k" }
-local G_ = { forward = "G", backward = "G" }
-local gg = { forward = "gg", backward = "gg" }
-local wb = { forward = "w", backward = "b" }
-local WB = { forward = "W", backward = "B" }
-local e_ge = { forward = "e", backward = "ge" }
-local E_gE = { forward = "E", backward = "gE" }
-local parens = { forward = ")", backward = "(" }
-local braces = { forward = "}", backward = "{" }
-local fF = { forward = "f", backward = "F" }
-local tT = { forward = "t", backward = "T" }
-local squares = { forward = "]", backward = "[" }
-
-local count_motion = {
-  l = lh,
-  h = lh,
-  j = jk,
-  k = jk,
-  G = G_,
-  gg = gg,
-  w = wb,
-  b = wb,
-  W = WB,
-  B = WB,
-  e = e_ge,
-  ge = e_ge,
-  E = E_gE,
-  gE = E_gE,
-  [")"] = parens,
-  ["("] = parens,
-  ["}"] = braces,
-  ["{"] = braces,
-}
-
-local count_motion_char = {
-  f = fF,
-  F = fF,
-  t = tT,
-  T = tT,
-  ["]"] = squares,
-  ["["] = squares,
-}
-
 ---@class MotionSpec
+---@field mode string[]
 ---@field forward string
 ---@field backward string
+---@field has_char_argument boolean|nil
+
+---@class CapturedMotion
+---@field motion MotionSpec
 ---@field char string|nil
 ---@field count integer|nil
-local motion_spec = {}
 
--- Store a potential g motion.
----@type string
-local g_motion = ""
+---@type MotionSpec[]
+local MOTION_SPECS = {
+  { mode = { "n", "x" }, forward = "l", backward = "h" },
+  { mode = { "n", "x" }, forward = "j", backward = "k" },
+  { mode = { "n", "x" }, forward = "G", backward = "G" },
+  { mode = { "n", "x" }, forward = "gg", backward = "gg" },
+  { mode = { "n", "x" }, forward = "w", backward = "b" },
+  { mode = { "n", "x" }, forward = "W", backward = "B" },
+  { mode = { "n", "x" }, forward = "e", backward = "ge" },
+  { mode = { "n", "x" }, forward = "E", backward = "gE" },
+  { mode = { "n", "x" }, forward = ")", backward = "(" },
+  { mode = { "n", "x" }, forward = "}", backward = "{" },
+  { mode = { "x" }, forward = "an", backward = "in" },
+  { mode = { "n", "x" }, forward = "f", backward = "F", has_char_argument = true },
+  { mode = { "n", "x" }, forward = "t", backward = "T", has_char_argument = true },
+  { mode = { "n", "x" }, forward = "]", backward = "[", has_char_argument = true },
+}
 
--- Store a pending char motion.
----@type string
-local char_motion = ""
+---@param motion_spec MotionSpec
+---@param nvim_mode string
+---@return boolean
+local function match_mode(motion_spec, nvim_mode)
+  nvim_mode = nvim_mode:sub(1, 1)
+  for _, motion_spec_mode in ipairs(motion_spec.mode) do
+    if motion_spec_mode == "n" and nvim_mode == "n" then
+      return true
+    elseif motion_spec_mode == "x" and (nvim_mode == "v" or nvim_mode == "V" or nvim_mode == "\22") then
+      return true
+    end
+  end
+  return false
+end
+
+---@param motion_spec MotionSpec
+---@param input_buffer string
+---@return boolean
+local function match_motion(motion_spec, input_buffer)
+  return input_buffer == motion_spec.forward or input_buffer == motion_spec.backward
+end
+
+local INPUT_BUFFER = ""
+local PENDING_CHAR_ARGUMENT = false
 
 -- Store the last motion to be repeated with ; and ,
----@type MotionSpec|nil
-local last_motion = nil
+---@type CapturedMotion|nil
+local captured_motion = nil
 
 vim.on_key(
   ---@param _ string
   ---@param typed string
   function(_, typed)
     typed = vim.fn.keytrans(typed)
+    local mode = vim.api.nvim_get_mode().mode
 
-    ---@type MotionSpec|nil
-    local motion = nil
+    -- If we are not pending char argument, we have to consider the typed key now.
+    -- Otherwise, we look at the buffered keys to find out what the motion was.
+    if not PENDING_CHAR_ARGUMENT then
+      INPUT_BUFFER = INPUT_BUFFER .. typed
+    end
 
-    if g_motion ~= "" then
-      g_motion = g_motion .. typed
-      if count_motion[g_motion] ~= nil then
-        motion = {
-          forward = count_motion[g_motion].forward,
-          backward = count_motion[g_motion].backward,
-        }
+    for _, motion_spec in ipairs(MOTION_SPECS) do
+      if match_mode(motion_spec, mode) and match_motion(motion_spec, INPUT_BUFFER) then
+        -- The motion does not require a char argument, we can capture it immediately.
+        if motion_spec.has_char_argument == nil or motion_spec.has_char_argument == false then
+          captured_motion = {
+            motion = motion_spec,
+          }
+          if vim.v.count ~= 0 then
+            captured_motion.count = vim.v.count
+          end
+          goto cleanup
+        end
+
+        -- Otherwise, the motion require a char argument.
+        -- We were waiting already.
+        if PENDING_CHAR_ARGUMENT then
+          captured_motion = {
+            motion = motion_spec,
+            char = typed,
+          }
+          if vim.v.count ~= 0 then
+            captured_motion.count = vim.v.count
+          end
+          goto cleanup
+        end
+
+        -- Otherwise, wait for the next char.
+        PENDING_CHAR_ARGUMENT = true
+        goto wait_for_next_key
       end
-      g_motion = ""
-    elseif char_motion ~= "" then
-      motion = {
-        forward = count_motion_char[char_motion].forward,
-        backward = count_motion_char[char_motion].backward,
-        char = typed,
-      }
-      char_motion = ""
-    elseif count_motion_char[typed] ~= nil then
-      char_motion = typed
-    elseif count_motion[typed] ~= nil then
-      motion = {
-        forward = count_motion[typed].forward,
-        backward = count_motion[typed].backward,
-      }
-    elseif typed == "g" then
-      g_motion = "g"
     end
 
-    if vim.v.count ~= 0 and motion ~= nil then
-      motion.count = vim.v.count
-    end
+    -- The loop falls through.
+    -- This means the input keys match nothing.
+    -- We forget everything and start from scratch.
+    ::cleanup::
+    INPUT_BUFFER = ""
+    PENDING_CHAR_ARGUMENT = false
 
-    if motion ~= nil then
-      last_motion = motion
-    end
+    ::wait_for_next_key::
   end
 )
 
 ---@param direction "forward"|"backward"
 local function repeat_last_motion(direction)
-  if last_motion == nil then
+  if captured_motion == nil then
     return
   end
 
-  local command = last_motion[direction]
-  if last_motion.count ~= nil then
-    command = last_motion.count .. command
+  local command = captured_motion.motion[direction]
+  if captured_motion.count ~= nil then
+    command = captured_motion.count .. command
   end
-  if last_motion.char ~= nil then
-    command = command .. last_motion.char
+  if captured_motion.char ~= nil then
+    command = command .. captured_motion.char
   end
 
-  -- This plugin does not support mapping (yet).
-  local vim_cmd = [[normal! ]] .. command
-  vim.cmd(vim_cmd)
+  -- m: remap keys so that mappings like [h (from gitsigns) work
+  -- t: treat the keys as if typed by the user. We want ; and , work like typing the command again.
+  -- x: execute all commands.
+  vim.api.nvim_feedkeys(command, "mtx", true)
 end
 
 vim.keymap.set({ "n", "x" }, ";", function()
