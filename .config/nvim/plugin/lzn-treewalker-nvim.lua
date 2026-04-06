847f1044a14b27a55cceaec520f6f1b8828c045d
Louis Chan <louischan0325@gmail.com>
2026-04-06 20:47:43 +0800

    Remove treesitter fix for treewalker
    As of 2026-04-06, parser:parse() is called properly now on latest main branch.

--- .config/nvim/plugin/lzn-treewalker-nvim.lua
+++ .config/nvim/plugin/lzn-treewalker-nvim.lua
@@ -7,14 +7,13 @@ local setup = once(function()
     highlight_group = "IncSearch",
   })
 
-  local fix_treesitter_function = require("fix_treesitter_function")
   local make_repeatable = require("dot-repeat").make_repeatable
 
   local actions = {
-    swap_up = make_repeatable("treewalker_swap_up", fix_treesitter_function(treewalker.swap_up)),
-    swap_right = make_repeatable("treewalker_swap_right", fix_treesitter_function(treewalker.swap_right)),
-    swap_down = make_repeatable("treewalker_swap_down", fix_treesitter_function(treewalker.swap_down)),
-    swap_left = make_repeatable("treewalker_swap_left", fix_treesitter_function(treewalker.swap_left)),
+    swap_up = make_repeatable("treewalker_swap_up", treewalker.swap_up),
+    swap_right = make_repeatable("treewalker_swap_right", treewalker.swap_right),
+    swap_down = make_repeatable("treewalker_swap_down", treewalker.swap_down),
+    swap_left = make_repeatable("treewalker_swap_left", treewalker.swap_left),
   }
   return actions
 end)
