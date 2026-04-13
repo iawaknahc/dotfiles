756760ade466db262f4ca8c5dccf18949ace4add
Louis Chan <louischan0325@gmail.com>
2026-04-13 21:57:34 +0800

    Use a custom version of prelude

--- home-manager/numbat/default.nix
+++ home-manager/numbat/default.nix
@@ -1,12 +1,23 @@
-{ ... }:
+{ pkgs, config, ... }:
+let
+  configDir =
+    if pkgs.stdenv.hostPlatform.isDarwin then
+      "Library/Application Support/numbat"
+    else
+      "${config.xdg.configHome}/numbat";
+in
 {
   programs.numbat.enable = true;
   programs.numbat.initFile = ./init.nbt;
   programs.numbat.settings = {
     exchange-rates = {
-      # numbat support of currency is limited as of 2026-04-13
+      # Numbat's support for currency is limited as of 2026-04-13
       # See https://github.com/sharkdp/numbat/issues/438
+      # We set fetching-policy to "never" so that Numbat will not fetch and define currency units.
       fetching-policy = "never";
     };
   };
+  # Build our custom prelude to exclude dimension Money and the associated units.
+  # The overriding of prelude is documented at https://numbat.dev/docs/cli/customization/#startup
+  home.file."${configDir}/modules/prelude.nbt".source = ./modules/prelude.nbt;
 }
