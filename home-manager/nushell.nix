6c0c9d337426b87a7fd21c72122f96b82cdaac31
Louis Chan <louischan0325@gmail.com>
2026-04-17 11:39:37 +0800

    Enable nushell again

--- home-manager/nushell.nix
+++ home-manager/nushell.nix
@@ -1,9 +1,6 @@
 { pkgs, config, ... }:
 {
-  # nushell is broken on macOS, see https://github.com/NixOS/nixpkgs/issues/485915
-  # As of 2026-02-03, the fix is on master only https://github.com/NixOS/nixpkgs/pull/486297
-  # programs.nushell.enable = true;
-  programs.nushell.enable = false;
+  programs.nushell.enable = true;
   programs.nushell.shellAliases = config.home.shellAliases;
   programs.nushell.configFile.source = ../.config/nushell/config.nu;
   # Install official plugins.
