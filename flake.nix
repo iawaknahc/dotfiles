793feb45ceb7c5634826d48c16a0759e543d053f
Louis Chan <louischan0325@gmail.com>
2026-04-27 17:13:46 +0800

    Move machines to its own file

--- flake.nix
+++ flake.nix
@@ -61,27 +61,7 @@
       ...
     }:
     let
-      machines = [
-        {
-          hostname = "louischan-m4";
-          system = "aarch64-darwin";
-          username = "louischan";
-          homeDirectory = "/Users/louischan";
-        }
-        {
-          hostname = "louischan-work";
-          system = "aarch64-darwin";
-          username = "louischan";
-          homeDirectory = "/Users/louischan";
-        }
-        # NOTE(nixd): See ./.config/nvim/lua/plugins/nvim-lspconfig.lua for details.
-        {
-          hostname = "nixd";
-          system = "aarch64-darwin";
-          username = "nixd";
-          homeDirectory = "/homeless-shelter";
-        }
-      ];
+      machines = import ./machines.nix;
     in
     {
       tests = {
---
+++ machines.nix
@@ -0,0 +1,21 @@
+[
+  {
+    hostname = "louischan-m4";
+    system = "aarch64-darwin";
+    username = "louischan";
+    homeDirectory = "/Users/louischan";
+  }
+  {
+    hostname = "louischan-work";
+    system = "aarch64-darwin";
+    username = "louischan";
+    homeDirectory = "/Users/louischan";
+  }
+  # NOTE(nixd): See ./.config/nvim/lua/plugins/nvim-lspconfig.lua for details.
+  {
+    hostname = "nixd";
+    system = "aarch64-darwin";
+    username = "nixd";
+    homeDirectory = "/homeless-shelter";
+  }
+]
