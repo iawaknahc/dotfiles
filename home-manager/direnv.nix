75ac9e2dd38dc3fdf933b24ef033822db405a6c4
Louis Chan <louischan0325@gmail.com>
2026-04-27 15:33:02 +0800

    Work around zsh hangs indefinitely if it reads .zshrc

--- home-manager/direnv.nix
+++ home-manager/direnv.nix
@@ -1,15 +1,10 @@
 {
-  pkgs,
   config,
   lib,
   ...
 }:
 {
   programs.direnv.enable = true;
-  # FIXME: direnv fails to build. See https://github.com/NixOS/nixpkgs/issues/513019
-  programs.direnv.package = pkgs.direnv.overrideAttrs {
-    doCheck = false;
-  };
   programs.direnv.nix-direnv.enable = true;
   programs.direnv.enableBashIntegration = true;
   programs.direnv.enableFishIntegration = true;
--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -1,8 +1,35 @@
 # `zsh -i --login` reads ALL files in this order: .zprofile .zshrc .zlogin
 # `zsh -i` reads .zshrc
 
-{ lib, ... }:
+{ lib, pkgs, ... }:
 {
+  assertions = [
+    {
+      assertion =
+        builtins.convertHash {
+          hash = builtins.hashFile "sha256" "${pkgs.path}/pkgs/by-name/zs/zsh/package.nix";
+          hashAlgo = "sha256";
+          toHashFormat = "sri";
+        } == "sha256-hbaz9x1koNreZsMvh9DZ/ZjHqqjHcqyxVyC6lDTIu14=";
+      message = ''
+        https://github.com/NixOS/nixpkgs/blob/nixpkgs-unstable/pkgs/by-name/zs/zsh/package.nix has an update.
+        Consider whether the workaround for https://github.com/NixOS/nixpkgs/issues/513543#issuecomment-4321051141 is still needed or not.
+      '';
+    }
+  ];
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      # FIXME: zsh hangs if it reads .zshrc on start.
+      # See https://github.com/NixOS/nixpkgs/issues/513543#issuecomment-4321051141
+      # This bug also causes direnv to hang during its test.
+      # See https://github.com/NixOS/nixpkgs/issues/513019
+      zsh = prev.zsh.overrideAttrs (prevAttrs: {
+        configureFlags = prevAttrs.configureFlags ++ [ "zsh_cv_sys_sigsuspend=yes" ];
+      });
+    })
+  ];
+
   programs.zsh.enable = true;
   # In preparation for using neovim as default terminal program,
   # we disable vi mode in shell.
