8c489b58330890e7d5d326c273af1b818c8f4939
Louis Chan <louischan0325@gmail.com>
2026-04-30 23:21:33 +0800

    Install nu_plugin_regex

--- home-manager/nushell.nix
+++ home-manager/nushell.nix
@@ -42,5 +42,23 @@
         mainProgram = "nu_plugin_dt";
       };
     })
+    (pkgs.rustPlatform.buildRustPackage {
+      pname = "nu_plugin_regex";
+      version = "nushell-0.112.2";
+      src = pkgs.fetchFromGitHub {
+        owner = "fdncred";
+        repo = "nu_plugin_regex";
+        rev = "b13ad14b3e5fa333a92cb486dfe5ff3c8bf963c3";
+        hash = "sha256-6rBUkUuJAL+9ivHgDyfYWbyyHR9CAKjbjOoHrVBJfqg=";
+      };
+      doCheck = false;
+      cargoHash = "sha256-V/8ZCrzq2iJOE8h1BAsA4k0L4gYu8lslrYvuGsTs+mA=";
+      meta = {
+        description = "Nushell plugin to search text with regular expressions.";
+        homepage = "https://github.com/fdncred/nu_plugin_regex";
+        license = lib.licenses.mit;
+        mainProgram = "nu_plugin_regex";
+      };
+    })
   ];
 }
