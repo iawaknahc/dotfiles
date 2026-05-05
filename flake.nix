d0512ebde99133faa6337708d15a271d3e8a2350
Louis Chan <louischan0325@gmail.com>
2026-05-06 03:02:24 +0800

    Install flake-parts

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
     # Using channel has a higher possibility that the packages are available at https://cache.nixos.org
     nixpkgs-mine.url = "https://nixos.org/channels/nixpkgs-unstable/nixexprs.tar.xz";
 
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
     # platform-tools 37.0.0 was first available on 2026-03-03
     # However, the hash of https://dl.google.com/android/repository/platform-tools_r37.0.0-darwin.zip changed on 2026-04-15
     #
@@ -29,6 +31,7 @@
     nur = {
       url = "github:nix-community/NUR";
       inputs.nixpkgs.follows = "nixpkgs-mine";
+      inputs.flake-parts.follows = "flake-parts";
     };
     sops-nix = {
       url = "github:Mic92/sops-nix";
@@ -51,7 +54,8 @@
   };
 
   outputs =
-    {
+    inputs@{
+      flake-parts,
       nixpkgs-mine,
       android-nixpkgs,
       catppuccin,
@@ -66,94 +70,99 @@
       homebrew-cask,
       ...
     }:
-    let
-      machines = import ./machines.nix;
-    in
-    {
-      tests = {
-        md5toUUID = import ./lib/md5toUUID.test.nix;
-        userscript_metadata_block = import ./lib/userscript_metadata_block/default.test.nix;
-      };
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { ... }:
+      let
+        machines = import ./machines.nix;
+      in
+      {
+        systems = [ "aarch64-darwin" ];
 
-      formatter = nixpkgs-mine.lib.pipe machines [
-        (builtins.map (
-          { system, ... }:
+        perSystem =
+          { system, pkgs, ... }:
           {
-            ${system} = nixpkgs-mine.legacyPackages.${system}.nixfmt-tree;
-          }
-        ))
-        nixpkgs-mine.lib.attrsets.mergeAttrsList
-      ];
+            _module.args.pkgs = nixpkgs-mine.legacyPackages.${system};
+            formatter = pkgs.nixfmt-tree;
+          };
 
-      homeConfigurations = nixpkgs-mine.lib.pipe machines [
-        (builtins.map (
-          {
-            hostname,
-            system,
-            username,
-            homeDirectory,
-          }:
-          {
-            # home-manager will try homeConfigurations.username@hostname, and then homeConfigurations.username.
-            "${username}@${hostname}" = home-manager.lib.homeManagerConfiguration {
-              pkgs = nixpkgs-mine.legacyPackages.${system};
-              modules = [
-                {
-                  home.username = username;
-                  home.homeDirectory = homeDirectory;
-                }
-                ((import ./home-manager/nixPath.nix) {
-                  nixpkgs = nixpkgs-mine;
-                })
-                ((import ./home-manager/nixd.nix) {
-                  hostname = hostname;
-                })
-                ((import ./home-manager/catppuccin.nix) catppuccin)
-                ((import ./home-manager/nix-index-database.nix) nix-index-database)
-                ((import ./home-manager/sops-nix.nix) sops-nix)
-                ((import ./home-manager/mcp-servers-nix.nix) mcp-servers-nix)
-                ((import ./home-manager/nur.nix) nur)
-                ((import ./home-manager/android-nixpkgs.nix) android-nixpkgs)
-                ./home-manager
-              ];
-            };
-          }
-        ))
-        nixpkgs-mine.lib.attrsets.mergeAttrsList
-      ];
+        flake = {
+          tests = {
+            md5toUUID = import ./lib/md5toUUID.test.nix;
+            userscript_metadata_block = import ./lib/userscript_metadata_block/default.test.nix;
+          };
 
-      darwinConfigurations = nixpkgs-mine.lib.pipe machines [
-        (builtins.map (
-          {
-            hostname,
-            system,
-            username,
-            ...
-          }:
-          {
-            "${hostname}" = nix-darwin.lib.darwinSystem {
-              modules = [
-                {
-                  nixpkgs.hostPlatform = system;
-                  system.primaryUser = username;
-                }
-                ((import ./nix-darwin/homebrew.nix) {
-                  inherit nix-homebrew homebrew-core homebrew-cask;
-                })
-                ((import ./nix-darwin/karabiner.nix) nix-darwin)
-                ./nix-darwin
-              ];
-            };
-          }
-        ))
-        nixpkgs-mine.lib.attrsets.mergeAttrsList
-      ];
+          homeConfigurations = nixpkgs-mine.lib.pipe machines [
+            (builtins.map (
+              {
+                hostname,
+                username,
+                homeDirectory,
+                system,
+                ...
+              }:
+              {
+                # home-manager will try homeConfigurations.username@hostname, and then homeConfigurations.username.
+                "${username}@${hostname}" = home-manager.lib.homeManagerConfiguration {
+                  pkgs = nixpkgs-mine.legacyPackages.${system};
+                  modules = [
+                    {
+                      home.username = username;
+                      home.homeDirectory = homeDirectory;
+                    }
+                    ((import ./home-manager/nixPath.nix) {
+                      nixpkgs = nixpkgs-mine;
+                    })
+                    ((import ./home-manager/nixd.nix) {
+                      hostname = hostname;
+                    })
+                    ((import ./home-manager/catppuccin.nix) catppuccin)
+                    ((import ./home-manager/nix-index-database.nix) nix-index-database)
+                    ((import ./home-manager/sops-nix.nix) sops-nix)
+                    ((import ./home-manager/mcp-servers-nix.nix) mcp-servers-nix)
+                    ((import ./home-manager/nur.nix) nur)
+                    ((import ./home-manager/android-nixpkgs.nix) android-nixpkgs)
+                    ./home-manager
+                  ];
+                };
+              }
+            ))
+            nixpkgs-mine.lib.attrsets.mergeAttrsList
+          ];
 
-      nixosConfigurations.nas = nixpkgs-mine.lib.nixosSystem {
-        modules = [
-          ./nas
-          sops-nix.nixosModules.sops
-        ];
-      };
-    };
+          darwinConfigurations = nixpkgs-mine.lib.pipe machines [
+            (builtins.map (
+              {
+                hostname,
+                system,
+                username,
+                ...
+              }:
+              {
+                "${hostname}" = nix-darwin.lib.darwinSystem {
+                  modules = [
+                    {
+                      nixpkgs.hostPlatform = system;
+                      system.primaryUser = username;
+                    }
+                    ((import ./nix-darwin/homebrew.nix) {
+                      inherit nix-homebrew homebrew-core homebrew-cask;
+                    })
+                    ((import ./nix-darwin/karabiner.nix) nix-darwin)
+                    ./nix-darwin
+                  ];
+                };
+              }
+            ))
+            nixpkgs-mine.lib.attrsets.mergeAttrsList
+          ];
+
+          nixosConfigurations.nas = nixpkgs-mine.lib.nixosSystem {
+            modules = [
+              ./nas
+              sops-nix.nixosModules.sops
+            ];
+          };
+        };
+      }
+    );
 }
