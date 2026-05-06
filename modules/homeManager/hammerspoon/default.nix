badc74c32deea5e88422e21ce6063abfcbd79c54
Louis Chan <louischan0325@gmail.com>
2026-05-07 02:29:34 +0800

    Remove EmmyLua.spoon from this repository

--- modules/homeManager/hammerspoon/default.nix
+++ modules/homeManager/hammerspoon/default.nix
@@ -24,6 +24,19 @@
             final.nur.repos.natsukium.hammerspoon
           ];
         };
+        EmmyLua_spoon = prev.stdenv.mkDerivation {
+          name = "EmmyLua_spoon";
+          src = prev.fetchFromGitHub {
+            owner = "Hammerspoon";
+            repo = "Spoons";
+            rev = "5c20bcecc380acff5f0f5df7a718c5679aaaf62a";
+            hash = "sha256-LeMsmm/au7GbnETVAUNNABanjU1byXpYbzWG/5nT4sM=";
+          };
+          installPhase = ''
+            mkdir -p $out/share/Spoons
+            cp -R $src/Source/EmmyLua.spoon/. $out/share/Spoons/EmmyLua.spoon
+          '';
+        };
       }
     )
   ];
@@ -44,6 +57,10 @@
   ];
   home.file.".hammerspoon" = {
     source = ./hammerspoon;
+    recursive = true;
+  };
+  home.file.".hammerspoon/Spoons/EmmyLua.spoon" = {
+    source = "${pkgs.EmmyLua_spoon}/share/Spoons/EmmyLua.spoon";
     recursive = true;
   };
 }
