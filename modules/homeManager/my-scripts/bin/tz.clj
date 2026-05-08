04492512c4b6dca8aed2c5c1e8e7cecd01379fe1
Louis Chan <louischan0325@gmail.com>
2026-05-08 14:45:43 +0800

    Replace tz.clj with tz.py
    So that we can leverage our Python pipeline

--- modules/homeManager/my-scripts/bin/tz.clj
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bb
-
-(import java.time.ZoneId)
-(println (-> (ZoneId/systemDefault) (str)))
---
+++ modules/homeManager/my-scripts/bin/tz.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+import tzlocal
+
+print(tzlocal.get_localzone().key)
