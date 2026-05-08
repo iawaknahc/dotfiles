925bdddef762038c106decdf3194c072d79b56c7
Louis Chan <louischan0325@gmail.com>
2026-05-08 14:41:01 +0800

    Rename vimrc to vimrc.vim
    So that tokei can recognize it.

--- modules/homeManager/vim/default.nix
+++ modules/homeManager/vim/default.nix
@@ -3,5 +3,5 @@
   # The home-manager module programs.vim does not write $HOME/.vimrc.
   # So I did not use it.
   home.packages = with pkgs; [ vim-full ];
-  home.file.".vimrc".source = ./vimrc;
+  home.file.".vimrc".source = ./vimrc.vim;
 }
