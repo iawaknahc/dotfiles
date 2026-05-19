10bd47cffb482a3c142166bade23d489adfa49fe
Louis Chan <louischan0325@gmail.com>
2026-05-20 02:35:47 +0800

    Make hledger respect `LESS`

--- modules/homeManager/plain-text-accounting.nix
+++ modules/homeManager/plain-text-accounting.nix
@@ -1,7 +1,11 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   programs.ledger.enable = true;
   home.packages = with pkgs; [ hledger ];
+  home.sessionVariables = {
+    # hledger by default passes its opinionated set of flags to `less`.
+    HLEDGER_LESS = config.home.sessionVariables.LESS;
+  };
   xdg.configFile."hledger/hledger.conf".text = ''
     --strict
   '';
