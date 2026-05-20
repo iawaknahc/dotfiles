f1b9376c36c37cae5b25552f69c2c559c2d3db67
Louis Chan <louischan0325@gmail.com>
2026-05-20 20:34:20 +0800

    Configure `hledger check`

--- modules/homeManager/plain-text-accounting.nix
+++ modules/homeManager/plain-text-accounting.nix
@@ -9,7 +9,12 @@
     # hledger by default passes its opinionated set of flags to `less`.
     HLEDGER_LESS = config.home.sessionVariables.LESS;
   };
+  # `check payees` is turned off because I do not want to include `|` in every transaction description.
+  # `check uniqueleafnames` is turned off because I do want to have non-unique leaf account names.
   xdg.configFile."hledger/hledger.conf".text = ''
     --strict
+
+    [check]
+    tags ordereddates
   '';
 }
