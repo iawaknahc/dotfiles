23ea038aae58e3a3c9274265488acdeb2a066419
Louis Chan <louischan0325@gmail.com>
2026-06-01 01:32:54 +0800

    Install fava as a Python package

--- modules/homeManager/beancount/default.nix
+++ modules/homeManager/beancount/default.nix
@@ -6,6 +6,7 @@
         beancount
         beanquery
         beangulp
+        fava
 
         (config.mypython.pythonPackages.buildPythonPackage {
           pname = "my_plugins";
@@ -23,7 +24,8 @@
 
   home.packages = with pkgs; [
     beancount-language-server
+    # Ideally, beancount2ledger should be installed as a Python package.
+    # But it requires a specific version of setuptools which is incompatible with the version of setuptools on nixpkgs.
     config.mypython.pythonPackages.beancount2ledger
-    fava
   ];
 }
