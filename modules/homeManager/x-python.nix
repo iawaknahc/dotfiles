3a281ab29749eabfdd6601180136a751c40b410a
Louis Chan <louischan0325@gmail.com>
2026-06-05 14:37:06 +0800

    Wrap bean-format

--- modules/homeManager/x-python.nix
+++ modules/homeManager/x-python.nix
@@ -12,22 +12,35 @@
       description = "The version of Python, e.g 313, 314";
     };
 
+    mypython.pythonPackages = lib.mkOption {
+      type = lib.types.attrs;
+      default = pkgs."python${config.mypython.version}Packages";
+    };
+
     mypython.packages = lib.mkOption {
       type = lib.types.listOf (lib.types.functionTo (lib.types.listOf lib.types.package));
       default = [ ];
       description = "List of functions to select Python packages";
     };
 
-    mypython.pythonPackages = lib.mkOption {
-      type = lib.types.attrs;
-      default = pkgs."python${config.mypython.version}Packages";
+    mypython.packageFuncs = lib.mkOption {
+      type = lib.types.listOf (lib.types.functionTo lib.types.package);
+      default = [ ];
+      description = "List of functions to modify the resulting Python";
     };
 
     mypython.package = lib.mkOption {
       type = lib.types.package;
-      default = pkgs."python${config.mypython.version}".withPackages (
-        python-pkgs: (builtins.concatMap (selector: selector python-pkgs) config.mypython.packages)
-      );
+      default =
+        let
+          pythonWithPackagesInstalled = pkgs."python${config.mypython.version}".withPackages (
+            python-pkgs: (builtins.concatMap (selector: selector python-pkgs) config.mypython.packages)
+          );
+          pythonWithFuncsApplied = builtins.foldl' (
+            python: funcToModifyPython: funcToModifyPython python
+          ) pythonWithPackagesInstalled config.mypython.packageFuncs;
+        in
+        pythonWithFuncsApplied;
       description = "The resulting Python";
     };
   };
