cefe4fac7c1873831f27c5c1ac58e79aed7a192d
Louis Chan <louischan0325@gmail.com>
2026-06-07 20:00:14 +0800

    Replace fill-2-posting with simplify

--- modules/homeManager/my-scripts/bin/bean-refactor.py
+++ modules/homeManager/my-scripts/bin/bean-refactor.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 
 import argparse
+import copy
 import io
+from decimal import Decimal
 from typing import Callable, cast
 
 from autobean_refactor import models, parser, printer
 
+ZERO = Decimal("0")
+
 Subcommand = Callable[[argparse.Namespace], None]
 
 
-def cmd_fill_2_posting(args: argparse.Namespace):
+def cmd_simplify(args: argparse.Namespace):
     filepath = cast(str, args.filepath)
     with open(filepath, "r") as f:
         text = f.read()
@@ -19,24 +23,30 @@ def cmd_fill_2_posting(args: argparse.Namespace):
     for directive in file.directives:
         if isinstance(directive, models.Transaction):
             if len(directive.postings) == 2:
-                number = None
-                currency = None
-                num_omitted = 0
-                for posting in directive.postings:
-                    if isinstance(posting, models.Posting):
-                        if posting.number is None and posting.currency is None:
-                            num_omitted += 1
-                        elif (
-                            posting.number is not None and posting.currency is not None
-                        ):
-                            number = posting.number
-                            currency = posting.currency
-                if num_omitted == 1 and number is not None and currency is not None:
-                    for posting in directive.postings:
-                        if isinstance(posting, models.Posting):
-                            if posting.number is None and posting.currency is None:
-                                posting.number = -number
-                                posting.currency = currency
+                first_posting = cast(models.Posting, directive.postings[0])
+                second_posting = cast(models.Posting, directive.postings[1])
+                if (
+                    first_posting.cost is None
+                    and second_posting.cost is None
+                    and first_posting.price is None
+                    and second_posting.price is None
+                ):
+                    assert first_posting.number is not None
+                    if first_posting.number == ZERO:
+                        continue
+
+                    if first_posting.number < ZERO:
+                        first_posting, second_posting = (
+                            copy.deepcopy(second_posting),
+                            copy.deepcopy(first_posting),
+                        )
+                        directive.postings[0] = first_posting
+                        directive.postings[1] = second_posting
+
+                    new_second_posting = copy.deepcopy(second_posting)
+                    new_second_posting.number = None
+                    new_second_posting.currency = None
+                    directive.postings[1] = new_second_posting
 
     print(printer.print_model(file, io.StringIO()).getvalue())
 
@@ -46,11 +56,11 @@ def main():
     sub = ap.add_subparsers(dest="subcommand", required=True)
 
     p_fill = sub.add_parser(
-        "fill-2-posting",
-        help="Fill omitted posting amounts in simple 2-posting transactions",
+        "simplify",
+        help="Simplify the postings of simple transactions",
     )
     _ = p_fill.add_argument("filepath", help="Beancount file to process")
-    p_fill.set_defaults(func=cmd_fill_2_posting)
+    p_fill.set_defaults(func=cmd_simplify)
 
     args = ap.parse_args()
     cast(Subcommand, args.func)(args)
