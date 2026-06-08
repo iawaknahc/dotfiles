870deef176215d3cf1e38fa1a18e0e9ea825b551
Louis Chan <louischan0325@gmail.com>
2026-06-09 00:54:58 +0800

    Add some datetime related snippets

--- modules/homeManager/neovim/config/nvim/plugin/lzn-luasnip.lua
+++ modules/homeManager/neovim/config/nvim/plugin/lzn-luasnip.lua
@@ -5,3 +5,105 @@ require("luasnip.loaders.from_vscode").load({
     "./snippets",
   },
 })
+
+local ls = require("luasnip")
+local s, f = ls.snippet, ls.function_node
+
+---@return string
+local function today()
+  vim.cmd([[python <<EOF
+import datetime
+import vim
+
+d = datetime.date.today()
+vim.vars["python_string"] = d.isoformat()
+EOF
+]])
+  return tostring(vim.g.python_string)
+end
+
+---@return string
+local function yesterday()
+  vim.cmd([[python <<EOF
+import datetime
+import vim
+
+d = datetime.date.today() + datetime.timedelta(days=-1)
+vim.vars["python_string"] = d.isoformat()
+EOF
+]])
+  return tostring(vim.g.python_string)
+end
+
+---@return string
+local function tomorrow()
+  vim.cmd([[python <<EOF
+import datetime
+import vim
+
+d = datetime.date.today() + datetime.timedelta(days=1)
+vim.vars["python_string"] = d.isoformat()
+EOF
+]])
+  return tostring(vim.g.python_string)
+end
+
+---@return string
+local function thisweek()
+  vim.cmd([[python <<EOF
+import datetime
+import vim
+
+d = datetime.date.today()
+vim.vars["python_string"] = d.strftime("%G-W%V")
+EOF
+]])
+  return tostring(vim.g.python_string)
+end
+
+---@return string
+local function lastweek()
+  vim.cmd([[python <<EOF
+import datetime
+import vim
+
+d = datetime.date.today() + datetime.timedelta(weeks=-1)
+vim.vars["python_string"] = d.strftime("%G-W%V")
+EOF
+]])
+  return tostring(vim.g.python_string)
+end
+
+---@return string
+local function nextweek()
+  vim.cmd([[python <<EOF
+import datetime
+import vim
+
+d = datetime.date.today() + datetime.timedelta(weeks=1)
+vim.vars["python_string"] = d.strftime("%G-W%V")
+EOF
+]])
+  return tostring(vim.g.python_string)
+end
+
+ls.add_snippets("all", {
+  s("today", {
+    f(today, {}),
+  }),
+  s("yesterday", {
+    f(yesterday, {}),
+  }),
+  s("tomorrow", {
+    f(tomorrow, {}),
+  }),
+  s("thisweek", {
+    f(thisweek, {}),
+  }),
+  s("lastweek", {
+    f(lastweek, {}),
+  }),
+  s("nextweek", {
+    f(nextweek, {}),
+  }),
+})
