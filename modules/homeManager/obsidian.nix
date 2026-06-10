dbf9fa307febb00bf3fe464375b1a066d3efb446
Louis Chan <louischan0325@gmail.com>
2026-06-11 01:38:12 +0800

    Manage the installation of Obsidian with Nix

--- modules/homeManager/obsidian.nix
+++ modules/homeManager/obsidian.nix
@@ -1,5 +1,11 @@
-{ pkgs, config, ... }:
+{ config, ... }:
 {
+  # The settings ARE NOT managed by Nix.
+  # Obsidian stores settings in the vault.
+  # The vault is configured to be synchronized between devices with Syncthing.
+  programs.obsidian.enable = true;
+  programs.obsidian.cli.enable = true;
+
   sops.secrets."obsidian/google_places_api_key" = { };
   sops.templates."obsidian-google-places-api-key.sh" = {
     mode = "0700";
@@ -9,14 +15,4 @@
       export GOOGLE_API_KEY="${config.sops.placeholder."obsidian/google_places_api_key"}"
     '';
   };
-
-  home.packages =
-    if pkgs.stdenv.hostPlatform.isDarwin then
-      [
-        (pkgs.writeShellScriptBin "obsidian" ''
-          /Applications/Obsidian.app/Contents/MacOS/obsidian-cli "$@"
-        '')
-      ]
-    else
-      [ ];
 }
