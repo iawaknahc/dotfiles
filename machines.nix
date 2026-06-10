19dc3b43f5cff955005149a5b95d94b975c5c3ee
Louis Chan <louischan0325@gmail.com>
2026-06-11 02:44:06 +0800

    Delete a machine I no longer own

--- machines.nix
+++ machines.nix
@@ -5,10 +5,4 @@
     username = "louischan";
     homeDirectory = "/Users/louischan";
   }
-  {
-    hostname = "louischan-work";
-    system = "aarch64-darwin";
-    username = "louischan";
-    homeDirectory = "/Users/louischan";
-  }
 ]
