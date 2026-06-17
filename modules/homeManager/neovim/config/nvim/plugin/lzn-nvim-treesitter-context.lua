7f539bdbad509004f27710cdc691abdcdee87ce9
Louis Chan <louischan0325@gmail.com>
2026-06-18 00:09:27 +0800

    Limit the lines occupied by treesitter-context
    When I am editing a Nix file, I often need to turn off
    treesitter-context to see the code.

--- modules/homeManager/neovim/config/nvim/plugin/lzn-nvim-treesitter-context.lua
+++ modules/homeManager/neovim/config/nvim/plugin/lzn-nvim-treesitter-context.lua
@@ -1,3 +1,9 @@
+require("treesitter-context").setup({
+  enable = true,
+  max_lines = "10%", -- Limit the window showing the context to 10% of the height of the window showing the buffer.
+  multiline_threshold = 1, -- Show only one line for each context.
+})
+
 vim.keymap.set("n", "<Leader>c", "<Cmd>TSContext toggle<CR>", {
   desc = "TSContext: toggle",
 })
