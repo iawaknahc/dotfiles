522565b6e80e208eecdab78432c5f66594d78ecf
Louis Chan <louischan0325@gmail.com>
2026-06-18 04:03:00 +0800

    Configure LuaSnip

--- modules/homeManager/neovim/config/nvim/plugin/configure__blink-cmp.lua
+++ modules/homeManager/neovim/config/nvim/plugin/configure__blink-cmp.lua
@@ -91,8 +91,6 @@ require("blink.cmp").setup({
     ["<C-n>"] = { "select_next", "fallback_to_mappings" },
     ["<C-b>"] = { "scroll_documentation_up", "fallback" },
     ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-    ["<Tab>"] = { "snippet_forward", "fallback" },
-    ["<S-Tab>"] = { "snippet_backward", "fallback" },
   },
   cmdline = {
     -- Actually :h cmdline-completion is better than blink.cmp.
--- modules/homeManager/neovim/config/nvim/plugin/configure__luasnip.lua
+++ modules/homeManager/neovim/config/nvim/plugin/configure__luasnip.lua
@@ -87,6 +87,15 @@ EOF
   return tostring(vim.g.python_string)
 end
 
+ls.setup({
+  keep_roots = true,
+  link_roots = true,
+  link_children = true,
+  exit_roots = false,
+  update_events = { "InsertLeave", "TextChanged", "TextChangedI" },
+  region_check_events = { "InsertEnter" },
+})
+
 ls.add_snippets("all", {
   s("today", {
     f(today, {}),
@@ -107,3 +116,19 @@ ls.add_snippets("all", {
     f(nextweek, {}),
   }),
 })
+
+-- Make CTRL-K works like CTRL-Y
+vim.keymap.set({ "i", "s" }, "<C-K>", function()
+  if ls.expand_or_jumpable() then
+    ls.expand_or_jump()
+  else
+    require("blink.cmp").select_and_accept()
+  end
+end)
+
+-- Since CTRL-K jumps forward, it follows naturally that CTRL-J jumps backward.
+vim.keymap.set({ "i", "s" }, "<C-J>", function()
+  if ls.jumpable(-1) then
+    ls.jump(-1)
+  end
+end)
