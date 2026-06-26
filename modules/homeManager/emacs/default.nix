72f19754060af9e3b332307b2f04a2c3f8ba973f
Louis Chan <louischan0325@gmail.com>
2026-06-27 01:28:23 +0800

    Install GUI Emacs

--- modules/homeManager/emacs/default.nix
+++ modules/homeManager/emacs/default.nix
@@ -1,15 +1,10 @@
 { pkgs, ... }:
 {
   programs.emacs.enable = true;
-  programs.emacs.package = pkgs.emacs-nox;
+  programs.emacs.package = pkgs.emacs-pgtk;
   programs.emacs.extraPackages =
     emacsPackages: with emacsPackages; [
       catppuccin-theme
-
-      goto-chg
-      evil
-      evil-collection
-      evil-terminal-cursor-changer
     ];
   home.file.".emacs.d/init.el".source = ./emacs.d/init.el;
 }
--- modules/homeManager/emacs/emacs.d/init.el
+++ modules/homeManager/emacs/emacs.d/init.el
@@ -5,18 +5,6 @@
 (global-display-line-numbers-mode t)
 (setq-default display-line-numbers-width 4)
 
-(setq evil-want-keybinding nil)
-(setq evil-want-integration t)
-(setq etcc-use-blink nil)
-
-(require 'evil)
-(require 'evil-collection)
-(require 'evil-terminal-cursor-changer)
-
-(evil-mode 1)
-(evil-collection-init)
-(etcc-on)
-
 (load-theme 'catppuccin :no-confirm)
 (setq catppuccin-flavor 'mocha)
 (catppuccin-reload)
