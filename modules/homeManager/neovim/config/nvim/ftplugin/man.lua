d84ea846f61638c397b2cac8e5fd2f5bc1f05e3d
Louis Chan <louischan0325@gmail.com>
2026-07-07 22:00:20 +0800

    Reset 'statuscolumn' when used as man pager

---
+++ modules/homeManager/neovim/config/nvim/ftplugin/man.lua
@@ -0,0 +1,3 @@
+-- Follow the official runtime file to remove any UI elements that occupy extra columns.
+-- See https://github.com/neovim/neovim/blob/v0.12.4/runtime/ftplugin/man.vim#L20
+vim.cmd([[setlocal statuscolumn=]])
