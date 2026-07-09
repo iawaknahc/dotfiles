dc00c3078935eaec941ec70e00c96f3e9034891e
Louis Chan <louischan0325@gmail.com>
2026-07-09 23:00:24 +0800

    Only enable display-line-numbers-mode in prog-mode and text-mode

--- modules/homeManager/emacs/emacs.d/lisp/init-display-line-numbers-mode.el
+++ modules/homeManager/emacs/emacs.d/lisp/init-display-line-numbers-mode.el
@@ -1,6 +1,9 @@
 ;;; -*- lexical-binding: t -*-
 
-(global-display-line-numbers-mode t)
-(setq-default display-line-numbers-width 4)
+;; Only enable display-line-numbers-mode when editing programming language source code and text files.
+;; For example, if display-line-numbers-mode is enabled in the mu4e headers view, the column header and the column content will misalign.
+(dolist
+ (hook '(prog-mode-hook text-mode-hook))
+ (add-hook hook (lambda () (display-line-numbers-mode 1) (setq display-line-numbers-width 4))))
 
 (provide 'init-display-line-numbers-mode)
