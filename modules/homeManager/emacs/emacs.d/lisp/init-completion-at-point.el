68b9edfee7daa9b4b649c34dd52d84b2cddfb8f1
Louis Chan <louischan0325@gmail.com>
2026-07-14 03:04:22 +0800

    Configure cape-file

--- modules/homeManager/emacs/emacs.d/lisp/init-completion-at-point.el
+++ modules/homeManager/emacs/emacs.d/lisp/init-completion-at-point.el
@@ -44,8 +44,53 @@
  :config
  (corfu-popupinfo-mode 1))
 
+(defun my/get-filepath-before-point ()
+  "Looking before point and see if there is a filepath.
+  Return the filepath if found, otherwise nil."
+  (let ((end (point))
+        (bol (pos-bol))
+        (regexp (rx (or (seq (in "`\"'")
+                             (group-n 1
+                                      (or "/" "./" "../" "~/")
+                                      (zero-or-more (in "- !\"#$%&'()*+,./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"))))
+                        (seq (group-n 1
+                                      (or "/" "./" "../" "~/")
+                                      (zero-or-more (in  "-!\"#$%&'()*+,./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~")))))))
+        result)
+    (save-excursion
+     (goto-char bol)
+     (while (re-search-forward regexp end t)
+       (when (= (match-end 0) end)
+         (setq result (buffer-substring-no-properties (match-beginning 1) (match-end 1)))))
+     result)))
+
+(defun my/expand-filepath-before-point ()
+  "Expand the filepath before point to an absolute path.
+  The filepath may or may not exist."
+  (let* ((filepath (my/get-filepath-before-point))
+         (base-directory (or (and buffer-file-name (file-name-directory buffer-file-name)) default-directory)))
+    (if filepath
+      (expand-file-name filepath base-directory))))
+
+(defun my/resolve-directory-before-point ()
+  "Resolve the filepath representing a directory before point.
+  Return /homeless-shelter if no directory is found."
+  (let* ((filepath (my/expand-filepath-before-point)))
+    (if filepath
+      (or (and (file-directory-p filepath) filepath)
+          (and (file-directory-p (file-name-directory filepath)) (file-name-directory filepath))
+          "/homeless-shelter"))))
+
+(defun my/cape-after-change-major-mode-hook ()
+  (add-hook 'completion-at-point-functions #'cape-file nil t))
+
 (use-package
- cape)
+ cape
+ :hook (after-change-major-mode-hook . my/cape-after-change-major-mode-hook)
+ :config
+ (setq
+  cape-file-directory #'my/resolve-directory-before-point
+  cape-file-prefix '("/" "~/" "./" "../")))
 
 (defun my/elisp-completion-at-point-annotation-function (candidate)
   "Annotate CANDIDATE assuming that CANDIDATE is a string that can be converted to an Elisp symbol."
