5e38ba59f7d61d91dd78dacd337f69e34c0e6113
Louis Chan <louischan0325@gmail.com>
2026-07-15 00:13:38 +0800

    Configure Consult

--- modules/homeManager/emacs/emacs.d/lisp/init-completing-read.el
+++ modules/homeManager/emacs/emacs.d/lisp/init-completing-read.el
@@ -3,7 +3,21 @@
 (use-package
  vertico
  :custom
- (vertico-preselect 'prompt)
+ ;; I tried a few combinations:
+ ;; 1. Install vertico-posframe. But the frame becomes invisible if I switch to another Application on macOS.
+ ;; 2. Use vertico-multiform and vertico-buffer. But it becomes overwhelming that I need to choose which command use which configuration.
+ ;;
+ ;; I ended up increase vertico-count from 10 to 20 to make the minibuffer taller to show more candidates.
+ ;; This is important when using consult-buffer, it contains section headers so a few more lines are needed.
+ ;; It is also important to set vertico-resize to nil to make the minibuffer height fixed.
+ ;; I can expect the minibuffer prompt always appear at the same line on the screen.
+ (vertico-count 20)
+ (vertico-resize nil)
+ ;; Previously I set vertico-preselect to prompt.
+ ;; But in consult-buffer, if the prompt is preselect, the preview is not the current buffer.
+ ;; This is very annoying.
+ ;; Also, unlike Corfu, in the minibuffer, I am expected to select something, so it is okay to preselect the first choice.
+ (vertico-preselect 'first)
  :config
  (vertico-mode 1))
 
--- modules/homeManager/emacs/emacs.d/lisp/init-consult.el
+++ modules/homeManager/emacs/emacs.d/lisp/init-consult.el
@@ -2,7 +2,24 @@
 
 (use-package
  consult
- :ensure nil)
+ :ensure nil
+ :bind
+  ;; C-x b
+ (([remap switch-to-buffer] . consult-buffer)
+  ;; C-x 4 b
+  ([remap switch-to-buffer-other-window] . consult-buffer-other-window)
+  ;; C-x 5 b
+  ([remap switch-to-buffer-other-frame] . consult-buffer-other-frame)
+  ;; C-x t b
+  ([remap switch-to-buffer-other-tab] . consult-buffer-other-tab)
+  ;; C-x r b
+  ([remap bookmark-jump] . consult-bookmark)
+  ;; M-y
+  ([remap yank-pop] . consult-yank-pop)
+  ;; M-g g or M-g M-g
+  ([remap goto-line] . consult-goto-line)
+  ;; C-x p b
+  ([remap project-switch-to-buffer] . consult-project-buffer)))
 
 (use-package
  embark-consult
