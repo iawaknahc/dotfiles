3cf4a4f8a1a20fa9d4e1bc197c006b8ed6c03bf0
Louis Chan <louischan0325@gmail.com>
2026-07-15 15:06:39 +0800

    Tidy up init-display-line-numbers-mode.el

--- modules/homeManager/emacs/emacs.d/lisp/init-display-line-numbers-mode.el
+++ modules/homeManager/emacs/emacs.d/lisp/init-display-line-numbers-mode.el
@@ -2,11 +2,11 @@
 
 (use-package
  emacs
- :config
+ :custom
+ (display-line-numbers-width 4)
  ;; Only enable display-line-numbers-mode when editing programming language source code and text files.
  ;; For example, if display-line-numbers-mode is enabled in the mu4e headers view, the column header and the column content will misalign.
- (dolist
-  (hook '(prog-mode-hook text-mode-hook markdown-ts-mode-hook))
-  (add-hook hook (lambda () (display-line-numbers-mode 1) (setq display-line-numbers-width 4)))))
+ :hook ((prog-mode . display-line-numbers-mode)
+        (text-mode . display-line-numbers-mode)))
 
 (provide 'init-display-line-numbers-mode)
