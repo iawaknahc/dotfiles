7fe99aeb0c574c0f6341e9862711f51bcfce9541
Louis Chan <louischan0325@gmail.com>
2026-07-17 03:38:12 +0800

    Use the default keybindings of GNU Info

--- modules/homeManager/gnuinfo/default.nix
+++ modules/homeManager/gnuinfo/default.nix
@@ -4,9 +4,4 @@
   # The module sets home.extraOutputsToInstall so it is better to use it,
   # than to installing the package directly.
   programs.info.enable = true;
-
-  home.file.".infokey" = {
-    enable = true;
-    source = ./infokey;
-  };
 }
