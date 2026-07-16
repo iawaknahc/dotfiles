0586f663529d8501a9acb00b61522a7e41718581
Louis Chan <louischan0325@gmail.com>
2026-07-16 13:09:49 +0800

    Make Orderless work better with Corfu

--- modules/homeManager/emacs/emacs.d/lisp/init-completion-styles.el
+++ modules/homeManager/emacs/emacs.d/lisp/init-completion-styles.el
@@ -12,7 +12,12 @@
  ;; But it is not a big deal because I do not complete email, at least for now.
  ;;
  ;; For file, Orderless cannot be used because we need to consider the compatibility with TRAMP.
- (completion-category-defaults '((file (styles partial-completion)))))
+ (completion-category-defaults '((file (styles partial-completion))))
+ ;; Set the separators to be one or more SPC, underscore, or hyphen.
+ ;; The default is one or more SPC, which does not work well for Corfu with auto-completion.
+ ;; With underscore and hyphen added, I can type `foo_bar` to means `foo bar`.
+ ;; Note that corfu-separator is left unchanged and set to SPC by default.
+ (orderless-component-separator (rx (+ (in "- _")))))
 
 (provide 'init-completion-styles)
 ;;; init-completion-styles.el ends here
