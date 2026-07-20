1ba9c4b5e8e3d2e359cf6f7520b04e137739d290
Louis Chan <louischan0325@gmail.com>
2026-07-20 19:35:23 +0800

    Make consult-buffer ?p or ?B show only project buffers

--- modules/homeManager/emacs/emacs.d/lisp/init-consult.el
+++ modules/homeManager/emacs/emacs.d/lisp/init-consult.el
@@ -1,5 +1,9 @@
 ;;; init-consult.el --- init-consult.el -*- lexical-binding: t -*-
 ;;; Commentary:
+;;
+;; The :items of `consult-source-project-buffer' is replaced by
+;; a version to use `project-buffers'.
+;;
 ;;; Code:
 
 ;; C-x b
@@ -18,6 +22,20 @@
 (keymap-global-set "<remap> <goto-line>" #'consult-goto-line)
 ;; C-x p b
 (keymap-global-set "<remap> <project-switch-to-buffer>" #'consult-project-buffer)
+
+(defun my/consult-source-project-buffer-items-around (f &rest args)
+  "The :around of my/consult-source-project-buffer-items.
+Dynamically bind `consult-buffer-list-function' to use `project-buffers'.
+`apply' F with ARGS as usual."
+  (let ((consult-buffer-list-function (lambda () (project-buffers (project-current)))))
+    (apply f args)))
+
+(with-eval-after-load 'consult
+  (unless (fboundp #'my/consult-source-project-buffer-items)
+    (let ((f (plist-get consult-source-project-buffer :items)))
+      (defalias 'my/consult-source-project-buffer-items f)
+      (advice-add 'my/consult-source-project-buffer-items :around #'my/consult-source-project-buffer-items-around)
+      (plist-put consult-source-project-buffer :items #'my/consult-source-project-buffer-items))))
 
 (provide 'init-consult)
 ;;; init-consult.el ends here
--- modules/homeManager/emacs/emacs.d/lisp/init-project.el
+++ modules/homeManager/emacs/emacs.d/lisp/init-project.el
@@ -1,14 +1,16 @@
 ;;; init-project.el --- init-project.el -*- lexical-binding: t -*-
 ;;; Commentary:
-;;; Code:
-
-;; The default implementation of project-buffers looks at default-directory of the buffer.
-;; If the value of default-directory matches the project-root, then it is considered as a project buffer.
+;;
+;; The default implementation of project-buffers looks at `default-directory' of the buffer.
+;; If the value of `default-directory' matches `project-root', then it is considered as a project buffer.
 ;; This behavior is not desirable because when we hit C-x C-b the buffer list inherits the default-directory of the current buffer.
 ;; This makes the buffer list a project buffer of the current project.
 ;;
 ;; Given that special buffers like the buffer list, scratch has no file associated with them,
 ;; a simple heuristic is to also check buffer-file-name of the buffer.
+;;
+;;; Code:
+
 (defun my/project-buffers-filter-return (buffers)
   "An :filter-return advice to project-buffers.
 It calls function `buffer-file-name' for each buffer in BUFFERS,
