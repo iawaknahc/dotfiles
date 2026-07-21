3ea87bba20673264a778c26294c6fb97c2cc33e6
Louis Chan <louischan0325@gmail.com>
2026-07-22 00:44:24 +0800

    Fix marks disappear every few second in Buffer List

--- modules/homeManager/emacs/emacs.d/lisp/init-auto-revert-mode.el
+++ modules/homeManager/emacs/emacs.d/lisp/init-auto-revert-mode.el
@@ -6,10 +6,21 @@
  ;; Do not poll on systems that support notification.
  auto-revert-avoid-polling t
  auto-revert-check-vc-info t
- ;; Make it work for Dired buffers.
- global-auto-revert-non-file-buffers t)
+ ;; The documentation of `global-auto-revert-non-file-buffers' is for
+ ;; Global Auto-Revert Mode to operate on non-file buffers, including
+ ;; Dired buffers and Buffer List buffer.
+ ;; But it does not mention a caveat.
+ ;; The marks I place in the Buffer List buffer is reverted every
+ ;; `auto-revert-interval' seconds.
+ ;; This is definitely a bug!
+ global-auto-revert-non-file-buffers nil)
 
-(add-hook 'after-init-hook #'global-auto-revert-mode)
+;; Therefore, instead of turning on `global-auto-revert-mode',
+;; we turn on the local `auto-revert-mode' in file-visiting buffers,
+;; and Dired buffers.
+(add-hook 'prog-mode-hook #'auto-revert-mode)
+(add-hook 'text-mode-hook #'auto-revert-mode)
+(add-hook 'dired-mode-hook #'auto-revert-mode)
 
 (provide 'init-auto-revert-mode)
 ;;; init-auto-revert-mode.el ends here
