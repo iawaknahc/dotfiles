b76f9793258286c624579370c6f5582e7f7c865d
Louis Chan <louischan0325@gmail.com>
2026-07-27 16:43:06 +0800

    Remove tab bar layout

--- modules/homeManager/emacs/emacs.d/lisp/init-tab-bar.el
+++ modules/homeManager/emacs/emacs.d/lisp/init-tab-bar.el
@@ -2,45 +2,12 @@
 ;;; Commentary:
 ;;; Code:
 
-(defvar my-tab-bar-layout
-  '(("mu4e"    . my-tab-bar-email-tab)
-    ("*scratch*"  . my-tab-bar-scratch-tab)))
-
-(defun my-tab-bar-email-tab ()
-  "Construct the email tab."
-  (mu4e))
-
-(defun my-tab-bar-scratch-tab ()
-  "Construct the scratch buffer tab."
-  (switch-to-buffer (get-buffer "*scratch*")))
-
-(defun my-tab-bar-reset ()
-  "Reset all tabs to initial state."
-  (interactive)
-  (tab-bar-new-tab)
-  (tab-bar-rename-tab "placeholder")
-  ;; Close other tabs.
-  (dolist (tab (tab-bar-tabs))
-    (let ((name (alist-get 'name tab)))
-      (unless (equal name "placeholder")
-        (tab-bar-close-tab-by-name name))))
-  ;; Build the tabs.
-  (dolist (spec my-tab-bar-layout)
-    (tab-bar-new-tab)
-    (tab-bar-rename-tab (car spec))
-    (funcall (cdr spec)))
-  ;; Close the placeholder
-  (tab-bar-close-tab-by-name "placeholder")
-  ;; Select the first tab.
-  (tab-bar-select-tab-by-name (caar my-tab-bar-layout)))
-
 (setq
  ;; Show tab number.
  tab-bar-tab-hints t
  tab-bar-new-tab-choice "*scratch*")
 
 (tab-bar-mode 1)
-(add-hook 'after-init-hook #'my-tab-bar-reset)
 
 (provide 'init-tab-bar)
 ;;; init-tab-bar.el ends here
