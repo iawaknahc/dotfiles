9c92ef02c85e59974f43655cbf3b20960df165b9
Louis Chan <louischan0325@gmail.com>
2026-07-30 02:25:50 +0800

    Re-configure whitespace-mode
    I was incorrect on what whitespace-mode can do.

--- modules/homeManager/emacs/emacs.d/lisp/init-whitespace-mode.el
+++ modules/homeManager/emacs/emacs.d/lisp/init-whitespace-mode.el
@@ -6,11 +6,16 @@
 (add-hook 'prog-mode-hook #'whitespace-mode)
 (add-hook 'text-mode-hook #'whitespace-mode)
 
-(custom-set-faces
- '(whitespace-space ((t (:inherit line-number))))
- '(whitespace-hspace ((t (:inherit line-number))))
- '(whitespace-tab ((t (:inherit line-number))))
- '(whitespace-newline ((t (:inherit line-number)))))
+(defun my/whitespace-refresh-faces (&rest args)
+  "It gets the :background of `default' face.
+And then set it as :foreground to `whitespace-space' and `whitespace-tab'.
+ARGS is ignored."
+  (let* ((bg (face-attribute 'default :background)))
+    (custom-set-faces
+     `(whitespace-space ((t (:foreground ,bg))))
+     `(whitespace-tab ((t (:foreground ,bg)))))))
+(my/whitespace-refresh-faces)
+(add-hook 'enable-theme-functions #'my/whitespace-refresh-faces)
 
 (setq
  ;; Set this to a very large value so that long lines are not highlighted by default.
@@ -19,6 +24,14 @@
  ;; 2. (whitespace-mode -1)
  ;; 3. (whitespace-mode 1)
  whitespace-line-column 1000
+
+ ;; The two regexps are the same.
+ ;; The documentation says that the car is for SPACES and the cdr is for TABS.
+ ;; I tried to follow the documentation but I found TABS are not highlighted.
+ ;; Therefore, I made them the same, and specify `indentation' in `whitespace-style'.
+ ;; This combination finally highlight leading spaces and leading tabs.
+ whitespace-indentation-regexp (cons (rx line-start (group-n 1 (+ (in " \t")))) (rx line-start (group-n 1 (+ (in " \t")))))
+
  whitespace-style
  '(
    ;; According to the documentation, this symbol must be present for the rest to take effect.
@@ -31,25 +44,19 @@
    ;; Therefore, the 6 should be present together.
    ;;
    ;; Ideally, it is better to only have leading whitespaces visible, like Neovim listchars.
-   ;; The built-in whitespace-mode does not support this, and
-   ;; there is no popular package to implement this.
-   ;;
-   ;; So we made a compromise here.
-   ;; We enable the display of all whitespace characters everywhere.
-   ;; The default faces inherit from the face shadow, which is used to show comments.
-   ;; Having whitespace showing with the same color as comments is definitely unreadable.
-   ;;
-   ;; In the default group of faces, there are two candidates we may use, namely line-number and glyphless-char
-   ;; line-number is the face to draw the line numbers in display-line-numbers-mode.
-   ;; It is the most subtle face I can find in the default group.
-   ;; glyphless-char is the face to draw character that typically has no glyph.
-   ;; glyphless-char by default is (:height 0.6).
-   ;; So using it will cause whitespaces to be narrower.
-   ;; This is not desirable in editing code in monospace fonts.
-   ;; So we ended up using line-number.
+   ;; This is possible by the combination of the following:
+   ;; 1. Customize `whitespace-indentation-regexp' to target leading and trailing spaces and tabs.
+   ;;    Note that the spaces and the tabs must be in the first group.
+   ;; 2. Turn on space-mark and tab-mark, so that they are replaced by a visible glyph.
+   ;; 3. Customize face `whitespace-space' and `whitespace-tab'.
+   ;;    Specifically, we get the :background of `default' and set it as :foreground in the faces.
+   ;;    This makes the whitespaces invisible most of the time.
+   ;;    They are still visible in an active region.
+
    tabs ; The presence of this means use whitespace-tab to highlight tabs.
    spaces ; The presence of this means use whitespace-space and whitespace-hspace to highlight spaces.
    newline ; The presence of this means use whitespace-newline to highlight newlines.
+
    ;; I found it quite distracting to have newline shown.
    ;;newline-mark
    space-mark
@@ -60,10 +67,12 @@
    lines-tail ; This uses whitespace-line. It is an underline to show the line is too long.
    missing-newline-at-eof ; This uses whitespace-trailing.
    empty
-   indentation ; This uses whitespace-indentation. It has no background color.
+
    big-indent
-   space-after-tab ; This uses whitespace-space-after-tab.
-   space-before-tab; This uses whitespace-space-before-tab.
+
+   indentation
+   space-after-tab
+   space-before-tab
    ))
 
 (provide 'init-whitespace-mode)
