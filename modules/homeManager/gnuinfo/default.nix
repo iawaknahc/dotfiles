7c885016173aa3ed8aef1394b075f2af585d288b
Louis Chan <louischan0325@gmail.com>
2026-08-12 22:57:39 +0800

    Make all installed system Info manuals available in Emacs

--- modules/homeManager/gnuinfo/default.nix
+++ modules/homeManager/gnuinfo/default.nix
@@ -1,7 +1,13 @@
-{ ... }:
+{ config, ... }:
 {
   # GNU Info
   # The module sets home.extraOutputsToInstall so it is better to use it,
   # than to installing the package directly.
   programs.info.enable = true;
+  programs.emacs.extraConfig = ''
+    ;; Added in modules/homeManager/gnuinfo/default.nix
+    ;; It is to make all Info manuals installed in the Nix profile available in Emacs.
+    (with-eval-after-load 'info
+      (add-to-list 'Info-directory-list "${config.home.profileDirectory}/share/info"))
+  '';
 }
