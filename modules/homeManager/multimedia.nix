5aef51eb7f8c3575d199a32ef6d270edadc8d37e
Louis Chan <louischan0325@gmail.com>
2026-08-18 22:47:28 +0800

    Tidy up some FIXMEs

--- modules/homeManager/multimedia.nix
+++ modules/homeManager/multimedia.nix
@@ -6,7 +6,9 @@
     # Image
     exiftool
     imagemagick
-    # FIXME: On 2026-07-16, mat2 failed to build.
+    # FIXME: mat2 failed to build on macOS since around 2026-07-16
+    # See https://github.com/NixOS/nixpkgs/issues/546115
+    # See https://github.com/NixOS/nixpkgs/pull/546126
     # mat2 # https://0xacab.org/jvoisin/mat2
     tesseract
     qrencode # Write QR code
