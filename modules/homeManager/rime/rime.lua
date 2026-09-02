eee750b402200f85e4a1849b920e2dd0a0db9286
Louis Chan <louischan0325@gmail.com>
2026-09-02 23:12:10 +0800

    Add prefix U to type unicode character

--- modules/homeManager/rime/rime.lua
+++ modules/homeManager/rime/rime.lua
@@ -7,6 +7,98 @@ local function has_prefix(str, prefix)
   return string.sub(str, 1, #prefix) == prefix
 end
 
+---@param user_input string
+---@return string
+local function prepare_fts5_query(user_input)
+  local terms = {}
+  -- We treat all non-alphabetic characters as separators.
+  -- We do this because we are not using query argument binding,
+  -- so we want the query terms contain no special characters.
+  for term in string.gmatch(user_input, "[a-zA-Z0-9]+") do
+    table.insert(terms, term)
+  end
+  local column_filter = "{name tts} : "
+  return column_filter .. table.concat(terms, " ")
+end
+
+---@param cps string
+---@return string
+local function cps_to_string(cps)
+  local codepoints = {}
+  for codepoint_str in string.gmatch(cps, "[^ ]+") do
+    local codepoint = tonumber(codepoint_str, 16)
+    if codepoint ~= nil then
+      table.insert(codepoints, codepoint)
+    end
+  end
+  if table.unpack ~= nil then
+    return utf8.char(table.unpack(codepoints))
+  else
+    return utf8.char(unpack(codepoints))
+  end
+end
+
+---@param rows {cps: string, name: string}[]
+---@param segment Segment
+local function handle_rows(rows, segment)
+  for _, row in ipairs(rows) do
+    local name = row["name"]
+    local cps = row["cps"]
+    local str = cps_to_string(cps)
+    local candidate = Candidate("unicode", segment.start, segment._end, str, name)
+    yield(candidate)
+  end
+end
+
+---@param cmd string
+---@param segment Segment
+local function handle_cmd(cmd, segment)
+  local handle = io.popen(cmd, "r")
+  local stdout = handle:read("*a")
+  handle:close()
+
+  local ok, rows = pcall(json.decode, stdout)
+  if ok then
+    handle_rows(rows --[[@as {cps: string, name: string}[] ]], segment)
+  end
+end
+
+---@param codepoint integer
+---@param segment Segment
+local function search_by_codepoint(codepoint, segment)
+  if codepoint >= 0 and codepoint <= 0x10FFFF then
+    local cmd = string.format(
+      [[~/.nix-profile/bin/sqlite3 -json ~/.nix-profile/share/unicode/unicode.sqlite3 "select cps, name, tts from codepoint_sequence where cps = '%04X' limit 1;"]],
+      codepoint
+    )
+    handle_cmd(cmd, segment)
+  end
+end
+
+---@param query string
+---@param segment Segment
+local function search_by_fts5_query(query, segment)
+  local statement = string.format(
+    [[
+  WITH t AS (
+    SELECT cps, name, tts, rank FROM codepoint_sequence_trigram
+    WHERE codepoint_sequence_trigram MATCH '%s'
+    UNION
+    SELECT cps, name, tts, rank FROM codepoint_sequence_porter
+    WHERE codepoint_sequence_porter MATCH '%s'
+  )
+  SELECT cps, name, tts
+  FROM t
+  ORDER BY rank ASC
+  ]],
+    query,
+    query
+  )
+  local cmd =
+    string.format([[~/.nix-profile/bin/sqlite3 -json ~/.nix-profile/share/unicode/unicode.sqlite3 "%s"]], statement)
+  handle_cmd(cmd, segment)
+end
+
 ---@param input string
 ---@param segment Segment
 function unicode_translator(input, segment)
@@ -29,26 +121,12 @@ function unicode_translator(input, segment)
   if base ~= nil then
     local str = string.sub(input, 3)
     local codepoint = tonumber(str, base)
-    if codepoint ~= nil and codepoint >= 0 and codepoint <= 0x10FFFF then
-      local codepoint_str = utf8.char(codepoint)
-      local cmd = string.format(
-        [[~/.nix-profile/bin/sqlite3 -json ~/.nix-profile/share/unicode/unicode.sqlite3 "select * from codepoint_sequence where cps = '%04X' limit 1;"]],
-        codepoint
-      )
-      local handle = io.popen(cmd, "r")
-      local stdout = handle:read("*a")
-      ---@type {name: string}[]
-      local rows = json.decode(stdout)
-      handle:close()
-
-      if #rows > 0 then
-        local row = rows[1]
-        local name = row["name"]
-        if type(name) == "string" and name ~= "" then
-          local candidate = Candidate("unicode", segment.start, segment._end, codepoint_str, name)
-          yield(candidate)
-        end
-      end
+    if codepoint ~= nil then
+      search_by_codepoint(codepoint, segment)
     end
+  else
+    local str = string.sub(input, 2)
+    local query = prepare_fts5_query(str)
+    search_by_fts5_query(query, segment)
   end
 end
